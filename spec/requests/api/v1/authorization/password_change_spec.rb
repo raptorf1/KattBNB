08cf9d1b5817de011c49cba02f331704f2f7dcb1
raptorf1 <raptor_f1@hotmail.com>
2019-08-08 21:10:17 +0300

    Writes unit test for changing user password. Happy path only atm.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/requests/api/v1/authorization/password_change_spec.rb
+++ spec/requests/api/v1/authorization/password_change_spec.rb
@@ -1,7 +1,7 @@
-RSpec.describe 'User Account Deletion', type: :request do
+RSpec.describe 'User Changes Password', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
 
-  it 'returns a user and a token and then deletes user from the database' do
+  it 'returns a user and a token and then changes the user password' do
     expect(User.all.length).to eq 0
     post '/api/v1/auth', params: { email: 'zane@craft.se',
                                    password: 'password',
@@ -13,18 +13,27 @@ RSpec.describe 'User Account Deletion', type: :request do
     expect(json_response['status']).to eq 'success'
     expect(response.status).to eq 200
     expect(User.all.length).to eq 1
-    
+
     new_user_uid = response.headers['uid']
     new_user_token = response.headers['access-token']
     new_user_client = response.headers['client']
 
-    delete '/api/v1/auth', params: { uid: new_user_uid,
-                                     'access-token': new_user_token,
-                                     client: new_user_client
-                                   }, headers: headers
-
+    put '/api/v1/auth', params: { current_password: 'password',
+                                  password: '123456',
+                                  password_confirmation: '123456',
+                                  uid: new_user_uid,
+                                  'access-token': new_user_token,
+                                  client: new_user_client
+                                 }, headers: headers
     expect(json_response['status']).to eq 'success'
     expect(response.status).to eq 200
-    expect(User.all.length).to eq 0
+
+    post "/api/v1/auth/sign_in", params: { email: 'zane@craft.se',
+                                           password: '123456'
+                                         }, headers: headers
+ 
+    expect(response.status).to eq 200
+    expect(User.all.length).to eq 1
+
   end
 end
