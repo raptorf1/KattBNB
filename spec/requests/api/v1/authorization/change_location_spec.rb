247cb4f3e44123421b75d5b9af0aafd503e6a1c4
zanenkn <zane.neikena@gmail.com>
2019-08-10 13:07:59 +0200

    refactors and adjusts change_location and delete_account specs.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- spec/requests/api/v1/authorization/change_location_spec.rb
+++ spec/requests/api/v1/authorization/change_location_spec.rb
@@ -2,17 +2,19 @@ RSpec.describe 'User Changes Location Information and API', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
 
   it 'returns a user and a token and then changes the location' do
-    expect(User.all.length).to eq 0
+
     post '/api/v1/auth', params: { email: 'felix@craft.se',
                                    password: 'password',
                                    password_confirmation: 'password',
                                    nickname: 'FlavioB',
                                    location: 'Tokyo'
                                 }, headers: headers
-    expect(json_response['status']).to eq 'success'
-    expect(json_response['data']['location']).to eq 'Tokyo'
-    expect(response.status).to eq 200
-    expect(User.all.length).to eq 1
+    
+    User.last.update_attribute(:confirmed_at, '2019-08-10 09:56:34.588757')
+
+    post "/api/v1/auth/sign_in", params: { email: 'felix@craft.se',
+      password: 'password',
+    }, headers: headers
 
     new_user_uid = response.headers['uid']
     new_user_token = response.headers['access-token']
@@ -28,22 +30,6 @@ RSpec.describe 'User Changes Location Information and API', type: :request do
     expect(json_response['status']).to eq 'success'
     expect(response.status).to eq 200
     expect(json_response['data']['location']).to eq 'Japan'
-
-    post "/api/v1/auth/sign_in", params: { email: 'felix@craft.se',
-                                           password: 'password'
-                                         }, headers: headers 
-    expect(response.status).to eq 200
-    expect(User.all.length).to eq 1
-
-    expected_response = {
-      "data" => {
-        "id" => new_user_id, "uid" => 'felix@craft.se', "email" => 'felix@craft.se',
-        "provider" => "email", "name" => nil, "nickname" => 'FlavioB',
-        "location" => 'Japan', "image" => nil, "allow_password_change" => false
-      }
-    }
-
-    expect(json_response).to eq expected_response
   end
 
 end
