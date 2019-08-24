639af7ede01de9b800f7ad602c6fd5aa63bfae0b
raptorf1 <raptor_f1@hotmail.com>
2019-08-24 10:07:04 +0300

    Creates GET request spec
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ spec/requests/api/v1/host_profiles/index_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe Api::V1::HostProfilesController, type: :request do
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:another_user) { FactoryBot.create(:user, email: 'felix@craft.com', nickname: 'Planner') }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+  describe 'GET /api/v1/host_profiles' do
+
+    before do
+      profile_user = FactoryBot.create(:host_profile, user_id: user.id)
+      profile_another_user = FactoryBot.create(:host_profile, user_id: another_user.id)
+    end
+
+    it 'returns a collection of host profiles' do
+      get '/api/v1/host_profiles', headers: headers
+      expect(json_response.count).to eq 2
+    end
+
+    it 'returns 200 response' do
+      get '/api/v1/host_profiles', headers: headers
+      expect(response.status).to eq 200
+    end
+    
+    it 'has correct keys in the response' do
+      get '/api/v1/host_profiles', headers: headers
+
+      profiles = HostProfile.all
+
+      profiles.each do |profile|
+        expect(json_response[profiles.index(profile)]).to include('id')
+        expect(json_response[profiles.index(profile)]).to include('price_per_day_1_cat')
+        expect(json_response[profiles.index(profile)]).to include('supplement_price_per_cat_per_day')
+        expect(json_response[profiles.index(profile)]).to include('max_cats_accepted')
+        expect(json_response[profiles.index(profile)]).to include('availability')
+        expect(json_response[profiles.index(profile)]).to include('lat')
+        expect(json_response[profiles.index(profile)]).to include('long')
+      end
+    end
+
+    describe 'for a specific user' do
+      
+      it "responds with specific user's host profile" do
+        get '/api/v1/host_profiles', params: {user_id: another_user.id} 
+        user_profile = HostProfile.where(user_id: another_user.id)
+        expect(json_response[user_profile]['user']['id']).to eq another_user.id
+        expect(json_response.count).to eq 1
+      end
+    end
+  end
+end
