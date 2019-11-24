e4718d5f54ae1fba780a1a421d3fa229ef78decd
raptorf1 <raptor_f1@hotmail.com>
2019-11-24 23:24:09 +0200

    Converts javascript dates to ruby dates and displays them to the outbound email along with other useful booking information
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/mailers/bookings_mailer.rb
+++ app/mailers/bookings_mailer.rb
@@ -3,7 +3,9 @@ class BookingsMailer < ApplicationMailer
   def notify_host(host, booking)
     @booking = booking
     @host = host
-    mail(to: @host.email, subject: 'You have a new booking request')
+    @start_date = Time.at(booking.dates[0] / 1000)
+    @end_date = Time.at(booking.dates[booking.dates.length - 1] / 1000)
+    mail(to: @host.email, subject: 'You have a new booking request!')
   end
 
 end
