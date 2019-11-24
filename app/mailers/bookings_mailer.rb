78d1b2674f735634f7a46b10f681d4c2ecb32d6b
raptorf1 <raptor_f1@hotmail.com>
2019-11-24 19:55:02 +0200

    Passes in the booking as argument to the mailer method so that we can display booking info to the outbound email message
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/mailers/bookings_mailer.rb
+++ app/mailers/bookings_mailer.rb
@@ -1,6 +1,7 @@
 class BookingsMailer < ApplicationMailer
 
-  def notify_host(host)
+  def notify_host(host, booking)
+    @booking = booking
     @host = host
     mail(to: @host.email, subject: 'You have a new booking request')
   end
