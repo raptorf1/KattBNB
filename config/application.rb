63c9deacec220599d55873fb54b3c21075c17c78
raptorf1 <raptor_f1@hotmail.com>
2019-12-02 09:18:18 +0200

    Adds localhost as allowed origin for rack-cors
    Co-authored-by: zane <zane.neikena@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,14 @@ module KattBNBApi
           expose: %w(access-token expiry token-type uid client),
           max_age: 0
       end
+      allow do
+        origins 'localhost:3000'
+        resource '/api/v1/*', 
+          headers: :any, 
+          methods: %i[get post put patch delete],
+          expose: %w(access-token expiry token-type uid client),
+          max_age: 0
+      end
     end
   end
 end
