65408da3af0be59d8ea60764fb3afda5c4af319d
zanenkn <zane.neikena@gmail.com>
2019-12-08 14:36:10 +0100

    Unit tests for notify_user_accepted_booking.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- spec/mailers/bookings_spec.rb
+++ spec/mailers/bookings_spec.rb
@@ -1,10 +1,12 @@
 RSpec.describe BookingsMailer, type: :mailer do
-  describe 'notify_host_create_booking' do
-    let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-    let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-    let(:booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname) }
-    let(:new_request_mail) { BookingsMailer.notify_host_create_booking(host, booking, user) }
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let(:booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname) }
+  let(:new_request_mail) { BookingsMailer.notify_host_create_booking(host, booking, user) }
+  let(:accepted_request_mail) { BookingsMailer.notify_owner_accepted_booking(host, booking, user) }
 
+
+  describe 'notify_host_create_booking' do
     it 'renders the subject' do
       expect(new_request_mail.subject).to eql('You have a new booking request!')
     end
@@ -23,6 +25,25 @@ RSpec.describe BookingsMailer, type: :mailer do
       expect(new_request_mail.body.encoded).to match("#{user.nickname}")
       expect(new_request_mail.body.encoded).to match("#{booking.number_of_cats}")
     end
-
   end
+
+  describe 'notify_owner_accepted_booking' do
+    it 'renders the subject' do
+      expect(accepted_request_mail.subject).to eql('Your booking request got approved!')
+    end
+
+    it 'renders the receiver email' do
+      expect(accepted_request_mail.to).to eql([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(accepted_request_mail.from).to eql('KattBNB Notification Service')
+    end
+
+    it "contains basic booking information and host's & user's nicknames" do
+      expect(accepted_request_mail.body.encoded).to match("Hey #{user.nickname}!")
+      expect(accepted_request_mail.encoded).to match("#{host.nickname}")
+      expect(accepted_request_mail.encoded).to match("#{booking.number_of_cats}")
+    end
+  end  
 end
