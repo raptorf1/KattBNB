bbab3479e2db67a7c343fe07ded1a19ba632f9dd
zanenkn <zane.neikena@gmail.com>
2019-12-08 14:26:12 +0100

    Some re-wording of the existing unit tests for bookings mailer.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- spec/mailers/bookings_spec.rb
+++ spec/mailers/bookings_spec.rb
@@ -3,25 +3,25 @@ RSpec.describe BookingsMailer, type: :mailer do
     let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
     let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
     let(:booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname) }
-    let(:mail) { BookingsMailer.notify_host_create_booking(host, booking, user) }
+    let(:new_request_mail) { BookingsMailer.notify_host_create_booking(host, booking, user) }
 
     it 'renders the subject' do
-      expect(mail.subject).to eql('You have a new booking request!')
+      expect(new_request_mail.subject).to eql('You have a new booking request!')
     end
 
     it 'renders the receiver email' do
-      expect(mail.to).to eql([host.email])
+      expect(new_request_mail.to).to eql([host.email])
     end
 
     it 'renders the sender email' do
-      expect(mail.from).to eql('KattBNB Notification Service')
+      expect(new_request_mail.from).to eql('KattBNB Notification Service')
     end
 
     it "contains basic booking information and host's & user's nicknames" do
-      expect(mail.body.encoded).to match("Hey #{host.nickname}!")
-      expect(mail.body.encoded).to match("#{booking.message}")
-      expect(mail.body.encoded).to match("#{user.nickname}")
-      expect(mail.body.encoded).to match("#{booking.number_of_cats}")
+      expect(new_request_mail.body.encoded).to match("Hey #{host.nickname}!")
+      expect(new_request_mail.body.encoded).to match("#{booking.message}")
+      expect(new_request_mail.body.encoded).to match("#{user.nickname}")
+      expect(new_request_mail.body.encoded).to match("#{booking.number_of_cats}")
     end
 
   end
