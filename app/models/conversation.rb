27ee1d4035d54b395ee5e5643b67ea1a75ff25fe
zanenkn <zane.neikena@gmail.com>
2019-12-18 10:55:42 +0100

    Adds validations for Conversation model.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :user1, :class_name => 'User'
   belongs_to :user2, :class_name => 'User'
-  #validates_presence_of :number_of_cats, :message, :dates, :host_nickname, :status, :price_per_day, :price_total, :user_id
 
+  validates_presence_of :user1_id, :user2_id
 end
