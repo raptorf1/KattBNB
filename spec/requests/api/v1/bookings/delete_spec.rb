927b87304ba6bf842e2b7e9ab062f10e03cf9222
raptorf1 <raptor_f1@hotmail.com>
2019-12-19 10:35:20 +0200

    Creates basic spec for destroy action of bookings controller
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ spec/requests/api/v1/bookings/delete_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe Api::V1::HostProfilesController, type: :request do
+  let(:user) { FactoryBot.create(:user, email: 'george@mail.com', nickname: 'Alonso') }
+  let(:user2) { FactoryBot.create(:user, email: 'zane@mail.com', nickname: 'Kitten') }
+  let(:host_profile_user2) { FactoryBot.create(:host_profile, user_id: user2.id, availability: [1, 2, 3, 4, 5]) }
+  let(:booking) { FactoryBot.create(:booking, host_nickname: user2.nickname, user_id: user.id, dates: [1562889600000, 1562976000000]) }
+  let(:user_credentials) { user.create_new_auth_token }
+  let(:user2_credentials) { user2.create_new_auth_token }
+  let(:user_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+  let(:user2_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user2_credentials) }
+  let(:headers_no_auth) { { HTTP_ACCEPT: 'application/json' } }
+
+  describe 'DELETE /api/v1/bookings/id' do
+
+    it 'deletes the booking of the associated user and does not delete the user' do
+      delete "/api/v1/bookings/#{booking.id}", headers: user_headers
+      expect(response.status).to eq 200
+      expect(json_response['message']).to eq 'You have successfully deleted this booking'
+      expect(User.all.length).to eq 1
+      expect(User.last.present?).to eq true
+      expect(User.last.uid).to eq 'george@mail.com'
+      expect(Booking.all.length).to eq 0
+    end
+
+    it 'does not delete booking associated with another user' do
+      delete "/api/v1/bookings/#{booking.id}", headers: user2_headers
+      expect(response.status).to eq 422
+      expect(json_response['error']).to eq 'You cannot perform this action'
+    end
+
+    it 'does not delete any booking if user is not authenticated' do
+      delete "/api/v1/bookings/#{booking.id}", headers: headers_no_auth
+      expect(response.status).to eq 401
+      expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
+    end
+  end
+end
