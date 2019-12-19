8a560e2210ed2e840ec419156d988c4b20033029
raptorf1 <raptor_f1@hotmail.com>
2019-12-19 10:13:24 +0200

    Adjusts test to include availability change for host profile
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/tasks/cancel_after_3_days_spec.rb
+++ spec/tasks/cancel_after_3_days_spec.rb
@@ -1,8 +1,9 @@
 describe 'rake bookings:cancel_after_3_days', type: :task do
   let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
   let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:booking) { FactoryBot.create(:booking, created_at: 'Sat, 09 Nov 2019 09:06:48 UTC +00:00', user_id: user.id, host_nickname: host.nickname)}
-  let!(:booking2) { FactoryBot.create(:booking, created_at: Time.current, user_id: user.id, host_nickname: host.nickname)}
+  let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id, availability: [1562803200000, 1562889600000, 1562976000000, 1563062400000, 1563148800000]) }
+  let!(:booking) { FactoryBot.create(:booking, created_at: 'Sat, 09 Nov 2019 09:06:48 UTC +00:00', user_id: user.id, host_nickname: host.nickname, dates: [123, 456]) }
+  let!(:booking2) { FactoryBot.create(:booking, created_at: Time.current, user_id: user.id, host_nickname: host.nickname) }
 
   it 'emails the user and the host' do
     task.execute
@@ -15,6 +16,12 @@ describe 'rake bookings:cancel_after_3_days', type: :task do
 
   it 'runs gracefully with no errors' do
     expect { task.execute }.not_to raise_error
+  end
+
+  it 'adds back availability to host profile' do
+    task.execute
+    profile.reload
+    expect(profile.availability).to eq [123, 456, 1562803200000, 1562889600000, 1562976000000, 1563062400000, 1563148800000]
   end
 
   it 'logs to stdout' do
