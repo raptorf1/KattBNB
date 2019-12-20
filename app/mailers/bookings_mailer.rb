7fe972b4df81dfea7cd6b1e85727832bd876f8bc
raptorf1 <raptor_f1@hotmail.com>
2019-12-20 10:37:15 +0200

    Passes total price of booking to the mailer view
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/mailers/bookings_mailer.rb
+++ app/mailers/bookings_mailer.rb
@@ -70,6 +70,13 @@ class BookingsMailer < ApplicationMailer
     @start_date = Time.at(booking.dates[0] / 1000)
     @end_date = Time.at(booking.dates[booking.dates.length - 1] / 1000)
 
+    string_with_2_decimals = sprintf('%.2f', booking.price_total.to_s)
+    if string_with_2_decimals[string_with_2_decimals.length - 1] == '0' && string_with_2_decimals[string_with_2_decimals.length - 2] == '0'
+      @total = string_with_2_decimals.to_i
+    else
+      @total = sprintf('%.2f', string_with_2_decimals)
+    end
+
     mail(to: @host.email, subject: 'Pending booking information after account deletion request!')
   end
 
