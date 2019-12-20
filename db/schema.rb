08e47259a0cc810f97ecddffbd62f1b7ccf6f968
zanenkn <zane.neikena@gmail.com>
2019-12-20 19:42:00 +0100

    Generates the message model, specs and factory.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_091837) do
+ActiveRecord::Schema.define(version: 2019_12_20_183927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,16 @@ ActiveRecord::Schema.define(version: 2019_12_18_091837) do
     t.index ["user_id"], name: "index_host_profiles_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -97,4 +107,6 @@ ActiveRecord::Schema.define(version: 2019_12_18_091837) do
   add_foreign_key "conversations", "users", column: "user1_id"
   add_foreign_key "conversations", "users", column: "user2_id"
   add_foreign_key "host_profiles", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :message do
+    body { "MyString" }
+    user { nil }
+    conversation { nil }
+  end
+end
