d6ab683996057434c4c81d945118c1131c2bb177
zanenkn <zane.neikena@gmail.com>
2019-12-21 12:09:31 +0100

    Changes to the test to accomodate namespacing messages into the conversations.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- spec/requests/api/v1/messages/create_spec.rb
+++ spec/requests/api/v1/messages/create_spec.rb
@@ -1,15 +1,16 @@
-let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-let(:user2) { FactoryBot.create(:user, email: 'morechaos@thestreets.com', nickname: 'Harley Queen') }
-let!(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
-let(:credentials) { user1.create_new_auth_token }
-let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
-let(:not_headers) { {HTTP_ACCEPT: "application/json"} }
+RSpec.describe Api::V1::MessagesController, type: :request do
+  let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:user2) { FactoryBot.create(:user, email: 'morechaos@thestreets.com', nickname: 'Harley Queen') }
+  let!(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
+  let(:credentials) { user1.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+  let(:not_headers) { {HTTP_ACCEPT: "application/json"} }
 
-  describe 'POST /api/v1/messages' do
+  describe 'POST /api/v1/conversations/id/messages' do
 
     describe 'successfully' do
       before do
-        post '/api/v1/messages', params: {
+        post "/api/v1/conversations/#{conversation.id}/messages", params: {
           user_id: user1.id,
           conversation_id: conversation.id,
           body: 'Batman, u dedd.'
@@ -26,7 +27,7 @@ let(:not_headers) { {HTTP_ACCEPT: "application/json"} }
 
     describe 'unsuccessfully' do
       it 'Message cannot be created if user is not logged in' do
-        post '/api/v1/conversations', params: {
+        post "/api/v1/conversations/#{conversation.id}/messages", params: {
           user_id: user1.id,
           conversation_id: conversation.id,
           body: 'Batman, u dedd.'
