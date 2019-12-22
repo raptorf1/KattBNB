d039bc4e47674340d7015019f8061d831f449467
zanenkn <zane.neikena@gmail.com>
2019-12-22 11:11:40 +0100

    Another sad path scenario of user trying to create a message inside the conversation she is not associated with.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,14 +3,16 @@ class Api::V1::MessagesController < ApplicationController
   
   def create
     conversation = Conversation.find(params[:conversation_id])
-
-    message = conversation.message.create(message_params)
-    if message.persisted?
-      render json: { message: 'Successfully created', body: message.body }
+    if params[:user_id].to_i == conversation.user1_id || params[:user_id].to_i == conversation.user2_id
+      message = conversation.message.create(message_params)
+      if message.persisted?
+        render json: { message: 'Successfully created', body: message.body }
+      else
+        render json: { error: message.errors.full_messages }, status: 400
+      end
     else
-      render json: { error: review.errors.full_messages }, status: 400
-    end
-   
+      render json: { error: 'You cannot perform this action' }, status: 422
+    end 
   end
   
   private
--- spec/requests/api/v1/messages/create_spec.rb
+++ spec/requests/api/v1/messages/create_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe Api::V1::MessagesController, type: :request do
   let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
   let(:user2) { FactoryBot.create(:user, email: 'morechaos@thestreets.com', nickname: 'Harley Queen') }
+  let(:user3) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
   let!(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
-  let(:credentials) { user1.create_new_auth_token }
-  let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+  let(:credentials1) { user1.create_new_auth_token }
+  let(:headers1) { { HTTP_ACCEPT: "application/json" }.merge!(credentials1) }
+  let(:credentials3) { user3.create_new_auth_token }
+  let(:headers3) { { HTTP_ACCEPT: "application/json" }.merge!(credentials3) }
   let(:not_headers) { {HTTP_ACCEPT: "application/json"} }
 
   describe 'POST /api/v1/conversations/id/messages' do
@@ -15,7 +18,7 @@ RSpec.describe Api::V1::MessagesController, type: :request do
           conversation_id: conversation.id,
           body: 'Batman, u dedd.'
         },
-        headers: headers
+        headers: headers1
       end
 
       it 'creates a new message' do
@@ -34,6 +37,16 @@ RSpec.describe Api::V1::MessagesController, type: :request do
         },
         headers: not_headers
         expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
+      end
+
+      it 'Message cannot be created if user is not associated with the relevant conversation' do
+        post "/api/v1/conversations/#{conversation.id}/messages", params: {
+          user_id: user3.id,
+          conversation_id: conversation.id,
+          body: 'Joker, fuck you!'
+        },
+        headers: headers3
+        expect(json_response['error']).to eq 'You cannot perform this action'
       end
     end
   end
