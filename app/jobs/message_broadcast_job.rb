3ad1d8eccc9bf14d1e59e59121808cdfa4ebf9bc
zanenkn <zane.neikena@gmail.com>
2019-12-29 23:33:59 +0100

    i dont know wtf am i doing but i love it

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -4,7 +4,10 @@ class MessageBroadcastJob < ApplicationJob
     
     message = Message.find_by(id: message_id)
     if message
-      ActionCable.server.broadcast("conversations_#{message.conversation.id}_channel", message: message)
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        Messages::Serializer.new(message)
+      ).serializable_hash
+      ActionCable.server.broadcast("conversations_#{message.conversation.id}_channel", serialized_data)
     else
       puts("message not found with id: #{message_id}")
     end
