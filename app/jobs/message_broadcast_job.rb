4bbfa5b234808703e897ea345fc1efb7b9bbb640
raptorf1 <raptor_f1@hotmail.com>
2019-12-30 20:22:52 +0200

    Writes specs for MessageBroadcastJob. All 4 examples passing!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,15 +1,14 @@
 class MessageBroadcastJob < ApplicationJob
+  
   queue_as :messages
 
   def perform(message_id)
     message = Message.find_by(id: message_id)
     if message
-      serialized_data = ActiveModelSerializers::Adapter::Json.new(
-        Messages::Serializer.new(message)
-      ).serializable_hash
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(Messages::Serializer.new(message)).serializable_hash
       ActionCable.server.broadcast("conversations_#{message.conversation.id}_channel", serialized_data)
     else
-      puts("Message with id: #{message_id} not found")
+      return "Message with id #{message_id} not found"
     end
   end
 
---
+++ spec/jobs/message_broadcast_job_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe MessageBroadcastJob, :type => :job do
+  let!(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let!(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let!(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
+  let!(:message) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'Batman, I love you!') }
+
+  describe 'queue name' do
+    it 'to be messages' do
+      ActiveJob::Base.queue_adapter = :test
+      expect(MessageBroadcastJob.queue_name).to eq 'messages'
+    end
+  end
+
+  describe 'perform_later' do
+    it 'adds a job to the queue' do
+      ActiveJob::Base.queue_adapter = :test
+      expect { MessageBroadcastJob.perform_later }.to have_enqueued_job
+    end
+
+    it 'contains message id in the job arguments' do
+      ActiveJob::Base.queue_adapter = :test
+      expect(MessageBroadcastJob.perform_later(message.id).arguments).to eq [message.id]
+    end
+  end
+
+  describe 'perform_now' do
+    it 'displays error message if message id is invalid' do
+      ActiveJob::Base.queue_adapter = :test
+      expect(MessageBroadcastJob.perform_now(1000000)).to eq 'Message with id 1000000 not found'
+    end
+  end
+
+end
