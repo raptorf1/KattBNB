060c301eba4cfce537fb3afc961d423ee6ae7c8c
raptorf1 <raptor_f1@hotmail.com>
2019-12-31 20:37:28 +0200

    Happy path spec when message is broadcasted successfully
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/channels/conversations_channel_spec.rb
+++ spec/channels/conversations_channel_spec.rb
@@ -1,4 +1,9 @@
 RSpec.describe ConversationsChannel, type: :channel do
+  let!(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let!(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let!(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
+  let!(:message) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'Batman, I love you!') }
+
   before do
     stub_connection
   end
@@ -15,15 +20,22 @@ RSpec.describe ConversationsChannel, type: :channel do
   end
 
   it 'subscribes to a stream when conversation id is provided' do
-    subscribe(conversations_id: 42)
+    subscribe(conversations_id: conversation.id)
     expect(subscription).to be_confirmed
-    expect(subscription).to have_stream_from('conversations_42_channel')
+    expect(subscription).to have_stream_from("conversations_#{conversation.id}_channel")
   end
 
   it 'transmits relevant errors when message arguments are not within permitted params' do
-    subscribe(conversations_id: 42)
-    expect(subscription.send_message(conversation_id: 5)[1]['message']['type']).to eq 'errors'
-    expect(subscription.send_message(conversation_id: 5)[1]['message']['data']).to eq ["User must exist", "Conversation must exist", "Body can't be blank"]
+    subscribe(conversations_id: conversation.id)
+    expect(subscription.send_message(conversation_id: 5000000)[1]['message']['type']).to eq 'errors'
+    expect(subscription.send_message(conversation_id: 5000000)[1]['message']['data']).to eq ["User must exist", "Conversation must exist", "Body can't be blank"]
+  end
+
+  it 'broadcast message when message arguments are within permitted params' do
+    ActiveJob::Base.queue_adapter = :test
+    subscribe(conversations_id: conversation.id)
+    subscription.send_message(conversation_id: conversation.id, user_id: user1.id, body: message.body)
+    expect { MessageBroadcastJob.perform_later }.to have_enqueued_job
   end
 
 end
