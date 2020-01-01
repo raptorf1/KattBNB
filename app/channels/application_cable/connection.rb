1cb975d448d2558e579ae63f67fb5c651de1bd1d
raptorf1 <raptor_f1@hotmail.com>
2020-01-01 21:00:37 +0200

    Authenticates users that connect with action cable. Tests connection. Happy and sad paths.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,32 @@
-module ApplicationCable
-  class Connection < ActionCable::Connection::Base
+module ApplicationCable  
+  
+  class Connection < ActionCable::Connection::Base    
+    
+    identified_by :current_user
+
+    def connect      
+      self.current_user = find_verified_user 
+    end     
+
+
+    private      
+      def find_verified_user
+
+        if self.request.env['QUERY_STRING'] != nil
+          params = self.request.env['QUERY_STRING']
+          uid = params.split('uid=').last.split('&').first
+          token = params.split('token=').last.split('&').first
+          client_id = params.split('client=').last.split('&').first
+          user = User.find_by_uid(uid)
+          if user && user.valid_token?(token, client_id)          
+            user
+          else
+            reject_unauthorized_connection
+          end
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
+
 end
