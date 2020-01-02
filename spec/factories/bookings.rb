647f95941bb842860dce82235f804e4892189d32
raptorf1 <raptor_f1@hotmail.com>
2020-01-02 14:49:42 +0200

    Is this enough for 100%?
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :booking do
     association :user
-    number_of_cats { 1 }
-    message { "Yo mannnnnn!" }
+    number_of_cats { '1' }
+    message { 'Yo mannnnnn!' }
     dates { [1560, 1561, 1562, 1563] }
     status { 1 }
-    price_per_day { 156.36 }
-    price_total { 1550 }
-    host_nickname { "Boa" }
+    price_per_day { '156.36' }
+    price_total { '1550.2' }
+    host_nickname { 'Boa' }
   end
 end
