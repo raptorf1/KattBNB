318a84a9f08e7b8e8018ca01f1615706a095903a
raptorf1 <raptor_f1@hotmail.com>
2020-02-08 12:52:41 +0200

    Creates mailer for new messages
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ app/mailers/messages_mailer.rb
@@ -0,0 +1,11 @@
+class MessagesMailer < ApplicationMailer
+
+  def notify_user_new_message(user1, user2, conversation)
+    @user1 = user1
+    @user2 = user2
+    @conversation = conversation
+
+    mail(to: @user2.email, subject: "New message from #{@user1.nickname}!")
+  end
+
+end
