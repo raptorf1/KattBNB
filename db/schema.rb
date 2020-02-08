e6c90e6737b2a1345f4e8a8aa9cba786688af789
raptorf1 <raptor_f1@hotmail.com>
2020-02-08 12:34:21 +0200

    Generates new migration for User model. Adds new column to be used if user wants to be notified on every new message. Adjusts relevant test. Test passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_090611) do
+ActiveRecord::Schema.define(version: 2020_02_08_101926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_090611) do
     t.string "last_sign_in_ip"
     t.string "location"
     t.text "avatar"
+    t.boolean "message_notification", default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
