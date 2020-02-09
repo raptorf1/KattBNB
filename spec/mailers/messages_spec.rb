d623cfb3bae5c58d9c2316763fc5bc99685ab88e
raptorf1 <raptor_f1@hotmail.com>
2020-02-09 20:06:54 +0200

    The things I do for the 100%!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/mailers/messages_spec.rb
+++ spec/mailers/messages_spec.rb
@@ -3,7 +3,9 @@ RSpec.describe MessagesMailer, type: :mailer do
   let(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
   let(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
   let(:message) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'Something') }
+  let(:message2) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'SomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomething') }
   let(:new_message_mail) { MessagesMailer.notify_user_new_message(user1, user2, message.body) }
+  let(:new_message_mail2) { MessagesMailer.notify_user_new_message(user1, user2, message2.body) }
 
   describe 'notify_user_new_message' do
     it 'renders the subject' do
@@ -22,6 +24,7 @@ RSpec.describe MessagesMailer, type: :mailer do
       expect(new_message_mail.body.encoded).to match("Hey #{user2.nickname}!")
       expect(new_message_mail.body.encoded).to match("#{user1.nickname}")
       expect(new_message_mail.body.encoded).to match("#{message.body}")
+      expect(new_message_mail2.body.encoded).to match('SomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingS...')
     end
   end
 end
