0a8b39166ee1331b49489d9b3337720707752a92
raptorf1 <raptor_f1@hotmail.com>
2020-02-15 21:37:19 +0200

    Activates dependent destroy for messages when a conversation is deleted. Creates relevant spec. 262/262 examples passing!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ApplicationRecord
   belongs_to :user1, :class_name => 'User'
   belongs_to :user2, :class_name => 'User'
 
-  has_many :message
+  has_many :message, dependent: :destroy
 
   validates_presence_of :user1_id, :user2_id
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe Message, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:conversation) }
   end
+
+  describe 'Delete dependent setting' do
+    it 'message is deleted when associated conversation is deleted from the database' do
+      FactoryBot.create(:conversation)
+      FactoryBot.create(:message, conversation_id: Conversation.last.id)
+      expect(Conversation.all.length).to eq 1
+      expect(Message.all.length).to eq 1
+      Conversation.last.destroy
+      expect(Conversation.all.length).to eq 0
+      expect(Message.all.length).to eq 0
+    end
+  end
 end
