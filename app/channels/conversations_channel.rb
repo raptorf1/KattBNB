9625ebbe64bf4c992f75f12bc51ace2cc998ad43
raptorf1 <raptor_f1@hotmail.com>
2020-02-28 19:25:19 +0200

    Further improves extracted methods with ternaries. 278/278 examples passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -17,46 +17,10 @@ class ConversationsChannel < ApplicationCable::Channel
   def send_message(data)
     conversation = Conversation.find_by(id: data['conversation_id'])
     if conversation.hidden == nil
-      message = Message.create(conversation_id: data['conversation_id'], body: data['body'], user_id: data['user_id'])
-      attach_image(data, message)
-      if message.errors.present?
-        transmit({type: 'errors', data: message.errors.full_messages})
-      elsif conversation.user1_id == data['user_id'] || conversation.user2_id == data['user_id']
-        user_sending = User.where(id: message.user_id)
-        if conversation.user1_id == message.user_id
-          user_receiving = User.where(id: conversation.user2_id)
-        else
-          user_receiving = User.where(id: conversation.user1_id)
-        end
-        MessageBroadcastJob.perform_later(message.id)
-        if user_receiving[0].message_notification == true
-          MessagesMailer.notify_user_new_message(user_sending[0], user_receiving[0], message.body).deliver
-        end
-      else
-        message.destroy
-        transmit({type: 'errors', data: 'You cannot send message to a conversation you are not part of!'})
-      end
+      send_actions(data, conversation)
     else
       conversation.update_attribute('hidden', nil)
-      message = Message.create(conversation_id: data['conversation_id'], body: data['body'], user_id: data['user_id'])
-      attach_image(data, message)
-      if message.errors.present?
-        transmit({type: 'errors', data: message.errors.full_messages})
-      elsif conversation.user1_id == data['user_id'] || conversation.user2_id == data['user_id']
-        user_sending = User.where(id: message.user_id)
-        if conversation.user1_id == message.user_id
-          user_receiving = User.where(id: conversation.user2_id)
-        else
-          user_receiving = User.where(id: conversation.user1_id)
-        end
-        MessageBroadcastJob.perform_later(message.id)
-        if user_receiving[0].message_notification == true
-          MessagesMailer.notify_user_new_message(user_sending[0], user_receiving[0], message.body).deliver
-        end
-      else
-        message.destroy
-        transmit({type: 'errors', data: 'You cannot send message to a conversation you are not part of!'})
-      end
+      send_actions(data, conversation)
     end
   end
 
@@ -64,8 +28,22 @@ class ConversationsChannel < ApplicationCable::Channel
   private 
 
   def attach_image(data, message)
-    if data['image'] && data['image'].present?
-      DecodeImageService.attach_image(data['image'], message.image)
+    (data['image'] && data['image'].present?) && DecodeImageService.attach_image(data['image'], message.image)
+  end
+
+  def send_actions(data, conversation)
+    message = Message.create(conversation_id: data['conversation_id'], body: data['body'], user_id: data['user_id'])
+    attach_image(data, message)
+    if message.errors.present?
+      transmit({type: 'errors', data: message.errors.full_messages})
+    elsif conversation.user1_id == data['user_id'] || conversation.user2_id == data['user_id']
+      user_sending = User.where(id: message.user_id)
+      conversation.user1_id == message.user_id ? user_receiving = User.where(id: conversation.user2_id) : user_receiving = User.where(id: conversation.user1_id)
+      MessageBroadcastJob.perform_later(message.id)
+      user_receiving[0].message_notification == true && MessagesMailer.notify_user_new_message(user_sending[0], user_receiving[0], message.body).deliver
+    else
+      message.destroy
+      transmit({type: 'errors', data: 'You cannot send message to a conversation you are not part of!'})
     end
   end
 
