2b77e8be47f5d640efa3790da40cdd9b4a6a5ba5
raptorf1 <raptor_f1@hotmail.com>
2020-04-10 20:32:04 +0300

    Adjusts conversations and messages mailers to accept user lang pref. Adds test for conversations mailer. All green.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -6,7 +6,9 @@ class MessagesMailer < ApplicationMailer
 
     message == '' ? @message = I18n.t('serializers.conversations.index.image_attachment') : message.length > 100 ? @message = message.slice(0,100)+'...' : @message = message
 
-    mail(to: @user2.email, subject: I18n.t('mailers.messages.notify_user_new_message', user: @user1.nickname))
+    I18n.with_locale(@user2.lang_pref) do
+      mail(to: @user2.email, subject: I18n.t('mailers.messages.notify_user_new_message', user: @user1.nickname))
+    end
   end
 
 end
