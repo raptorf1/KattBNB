3bab8986ef98496fa7ee36b8209f46f5716cde5e
raptorf1 <raptor_f1@hotmail.com>
2020-04-20 16:38:29 +0300

    Creates performance tests for all mailer specs
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/mailers/conversations_spec.rb
+++ spec/mailers/conversations_spec.rb
@@ -1,9 +1,13 @@
+RSpec::Benchmark.configure do |config|
+  config.run_in_subprocess = true
+end
+
 RSpec.describe ConversationsMailer, type: :mailer do
   let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
   let(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
   let(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
-  let(:user3) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker SV', lang_pref: 'sv-SE') }
-  let(:user4) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman SV', lang_pref: 'sv-SE') }
+  let(:user3) { FactoryBot.create(:user, email: 'chaoss@thestreets.com', nickname: 'Joker SV', lang_pref: 'sv-SE') }
+  let(:user4) { FactoryBot.create(:user, email: 'orderr@thestreets.com', nickname: 'Batman SV', lang_pref: 'sv-SE') }
   let(:conversatio2) { FactoryBot.create(:conversation, user1_id: user3.id, user2_id: user4.id) }
   let(:new_conversation_mail) { ConversationsMailer.notify_user_new_conversation(user1, user2) }
   let(:new_conversation_mail2) { ConversationsMailer.notify_user_new_conversation(user3, user4) }
@@ -28,6 +32,16 @@ RSpec.describe ConversationsMailer, type: :mailer do
 
     it "contains users' nicknames in SV" do
       expect(new_conversation_mail2.body.encoded).to match("Hallå, #{user4.nickname}!")
+    end
+
+    it 'is performed under 500ms' do
+      expect { new_conversation_mail }.to perform_under(500).ms.sample(20).times
+      expect { new_conversation_mail2 }.to perform_under(500).ms.sample(20).times
+    end
+
+    it 'performs at least 800K iterations per second' do
+      expect { new_conversation_mail }.to perform_at_least(800000).ips
+      expect { new_conversation_mail2 }.to perform_at_least(800000).ips
     end
   end
 end
--- spec/mailers/messages_spec.rb
+++ spec/mailers/messages_spec.rb
@@ -1,9 +1,13 @@
+RSpec::Benchmark.configure do |config|
+  config.run_in_subprocess = true
+end
+
 RSpec.describe MessagesMailer, type: :mailer do
   let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
   let(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
   let(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
-  let(:user3) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker SV') }
-  let(:user4) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman SV', lang_pref: 'sv-SE') }
+  let(:user3) { FactoryBot.create(:user, email: 'chaoss@thestreets.com', nickname: 'Joker SV') }
+  let(:user4) { FactoryBot.create(:user, email: 'orderr@thestreets.com', nickname: 'Batman SV', lang_pref: 'sv-SE') }
   let(:conversation2) { FactoryBot.create(:conversation, user1_id: user3.id, user2_id: user4.id) }
   let(:message) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'Something') }
   let(:message2) { FactoryBot.create(:message, user_id: user1.id, conversation_id: conversation.id, body: 'SomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomethingSomething') }
@@ -34,6 +38,18 @@ RSpec.describe MessagesMailer, type: :mailer do
 
     it "contains users' nicknames in SV" do
       expect(new_message_mail3.body.encoded).to match("Hallå, #{user4.nickname}!")
+    end
+
+    it 'is performed under 500ms' do
+      expect { new_message_mail }.to perform_under(500).ms.sample(20).times
+      expect { new_message_mail2 }.to perform_under(500).ms.sample(20).times
+      expect { new_message_mail3 }.to perform_under(500).ms.sample(20).times
+    end
+
+    it 'performs at least 800K iterations per second' do
+      expect { new_message_mail }.to perform_at_least(800000).ips
+      expect { new_message_mail2 }.to perform_at_least(800000).ips
+      expect { new_message_mail3 }.to perform_at_least(800000).ips
     end
   end
 end
