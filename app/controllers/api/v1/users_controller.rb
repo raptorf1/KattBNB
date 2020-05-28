1fd56528e670efaa14369e7f36c3a00184f80aff
raptorf1 <raptor_f1@hotmail.com>
2020-05-28 18:07:40 +0300

    Completes user update spec. Happy and sad paths. All passing!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    if params[:client] != nil && params['access-token'] != nil
+    if params[:client] != nil && params['access-token'] != nil && params[:profile_avatar] != nil
       client = params[:client]
       token = params['access-token']
+      pic = params[:profile_avatar]
       if user.valid_token?(token, client)
-        attach_image(params[:profile_avatar], user)
+        attach_image(pic, user)
         render json: { message: I18n.t('controllers.reusable.update_success') }, status: 200
       else
         render json: { error: [I18n.t('controllers.reusable.update_error')] }, status: 422
--- spec/requests/api/v1/users/update_spec.rb
+++ spec/requests/api/v1/users/update_spec.rb
@@ -26,7 +26,9 @@ RSpec.describe 'User Saves / Changes Avatar and API', type: :request do
     new_user_client = response.headers['client']
     new_user_id = json_response['data']['id']
 
-    put "/api/v1/users/#{new_user_id}", params: {
+    expect(User.last.profile_avatar.attached?).to eq false
+
+    patch "/api/v1/users/#{new_user_id}", params: {
                                   profile_avatar: {
                                         type: "image/png",
                                         encoder: "name=carbon (5).png;base64",
@@ -36,30 +38,84 @@ RSpec.describe 'User Saves / Changes Avatar and API', type: :request do
                                   'access-token': new_user_token,
                                   client: new_user_client
                                }, headers: headers
-                               binding.pry
-    expect(json_response['status']).to eq 'success'
+    expect(json_response['message']).to eq 'Successfully updated!'
     expect(response.status).to eq 200
-    expect(json_response['data']['avatar']).to eq 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAABMbklEQVR42u1deXycVdV+7n1nSSaTvUnaJk33j'
+    expect(User.last.profile_avatar.attached?).to eq true
   end
 
+  it 'returns a user and a token but if token/client expires or is invalid does save the avatar' do
 
+    post '/api/v1/auth', params: { email: 'felix@craft.se',
+                                   password: 'password',
+                                   password_confirmation: 'password',
+                                   nickname: 'FlavioB',
+                                   location: 'Tokyo',
+                                   confirm_success_url: 'confirmed'
+                                }, headers: headers
+    
+    User.last.update_attribute(:confirmed_at, '2019-08-10 09:56:34.588757')
 
+    post "/api/v1/auth/sign_in", params: { email: 'felix@craft.se',
+                                           password: 'password',
+                                        }, headers: headers
 
+    new_user_uid = response.headers['uid']
+    new_user_token = response.headers['access-token']
+    new_user_client = response.headers['client']
+    new_user_id = json_response['data']['id']
 
+    expect(User.last.profile_avatar.attached?).to eq false
 
+    patch "/api/v1/users/#{new_user_id}", params: {
+                                  profile_avatar: {
+                                        type: "image/png",
+                                        encoder: "name=carbon (5).png;base64",
+                                        data: "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zf",
+                                        extension: "png" 
+                                      },
+                                  'access-token': 'kjhfvHTghjjhjbjk',
+                                  client: new_user_client
+                               }, headers: headers
+    expect(json_response['error']).to eq ['You cannot perform this action!']
+    expect(response.status).to eq 422
+    expect(User.last.profile_avatar.attached?).to eq false
+  end
 
+  it 'returns a user and a token but if no token/client params are passed raises error' do
 
+    post '/api/v1/auth', params: { email: 'felix@craft.se',
+                                   password: 'password',
+                                   password_confirmation: 'password',
+                                   nickname: 'FlavioB',
+                                   location: 'Tokyo',
+                                   confirm_success_url: 'confirmed'
+                                }, headers: headers
+    
+    User.last.update_attribute(:confirmed_at, '2019-08-10 09:56:34.588757')
 
+    post "/api/v1/auth/sign_in", params: { email: 'felix@craft.se',
+                                           password: 'password',
+                                        }, headers: headers
 
+    new_user_uid = response.headers['uid']
+    new_user_token = response.headers['access-token']
+    new_user_client = response.headers['client']
+    new_user_id = json_response['data']['id']
 
+    expect(User.last.profile_avatar.attached?).to eq false
 
-
-
-
-
-
-
-  
+    patch "/api/v1/users/#{new_user_id}", params: {
+                                  profile_avatar: {
+                                        type: "image/png",
+                                        encoder: "name=carbon (5).png;base64",
+                                        data: "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zf",
+                                        extension: "png" 
+                                      }
+                               }, headers: headers
+    expect(json_response['error']).to eq ['You cannot perform this action!']
+    expect(response.status).to eq 422
+    expect(User.last.profile_avatar.attached?).to eq false
+  end  
 
   it 'returns a user and a token and then saves the avatar in under 1 ms and with iteration rate of 5000000 per second' do
 
@@ -82,12 +138,17 @@ RSpec.describe 'User Saves / Changes Avatar and API', type: :request do
     new_user_client = response.headers['client']
     new_user_id = json_response['data']['id']
 
-    put_request = put '/api/v1/auth', params: { avatar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAABMbklEQVR42u1deXycVdV+7n1nSSaTvUnaJk33j',
-                                                uid: new_user_uid,
-                                               'access-token': new_user_token,
-                                                client: new_user_client
-                                             }, headers: headers
-    expect { put_request }.to perform_under(1).ms.sample(20).times
-    expect { put_request }.to perform_at_least(5000000).ips
+    patch_request = patch "/api/v1/users/#{new_user_id}", params: {
+      profile_avatar: {
+            type: "image/png",
+            encoder: "name=carbon (5).png;base64",
+            data: "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zf",
+            extension: "png" 
+          },
+      'access-token': new_user_token,
+      client: new_user_client
+   }, headers: headers
+    expect { patch_request }.to perform_under(1).ms.sample(20).times
+    expect { patch_request }.to perform_at_least(5000000).ips
   end
 end
