74bf7e73740d47b778db35216f22c8f453a87988
raptorf1 <raptor_f1@hotmail.com>
2020-05-28 18:46:32 +0300

    Edits all versions of user serializers to return url of blob attachment
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/serializers/users/serializer.rb
+++ app/serializers/users/serializer.rb
@@ -1,3 +1,12 @@
 class Users::Serializer < ActiveModel::Serializer
-  attributes :id, :location, :nickname, :avatar
+
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :location, :nickname
+  attribute :profile_avatar
+
+  def profile_avatar
+    object.profile_avatar.attached? ? (Rails.env.test? ? rails_blob_url(object.profile_avatar) : object&.profile_avatar&.service_url(expires_in: 1.hour, disposition: 'inline')) : nil
+  end
+
 end
