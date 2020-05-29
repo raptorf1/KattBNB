5dd2c176645ae398b55cc1dedba0e989d69be984
raptorf1 <raptor_f1@hotmail.com>
2020-05-29 20:49:49 +0300

    Sad path in case host is deleted
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/serializers/bookings/index_serializer.rb
+++ app/serializers/bookings/index_serializer.rb
@@ -23,7 +23,9 @@ class Bookings::IndexSerializer < ActiveModel::Serializer
 
   def host_avatar
     host = User.where(nickname: object.host_nickname)
-    host[0].profile_avatar.attached? ? (Rails.env.test? ? rails_blob_url(host[0].profile_avatar) : host[0]&.profile_avatar&.service_url(expires_in: 1.hour, disposition: 'inline')) : nil
+    unless host.length == 0
+      return host[0].profile_avatar.attached? ? (Rails.env.test? ? rails_blob_url(host[0].profile_avatar) : host[0]&.profile_avatar&.service_url(expires_in: 1.hour, disposition: 'inline')) : nil
+    end
   end
 
 end
