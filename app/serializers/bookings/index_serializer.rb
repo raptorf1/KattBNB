f34a1894983c4189713e71f1f6f62a65c376ec54
raptorf1 <raptor_f1@hotmail.com>
2020-06-02 16:38:25 +0300

    Edits bookings index serializer to not send out the whole review object
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/serializers/bookings/index_serializer.rb
+++ app/serializers/bookings/index_serializer.rb
@@ -2,10 +2,9 @@ class Bookings::IndexSerializer < ActiveModel::Serializer
 
   include Rails.application.routes.url_helpers
 
-  attributes :id, :number_of_cats, :dates, :status, :host_id, :host_profile_id, :host_location, :host_nickname, :message, :price_total, :host_message, :host_avatar, :host_description, :host_full_address, :host_real_lat, :host_real_long, :created_at, :updated_at, :user_id
+  attributes :id, :number_of_cats, :dates, :status, :host_id, :host_profile_id, :host_location, :host_nickname, :message, :price_total, :host_message, :host_avatar, :host_description, :host_full_address, :host_real_lat, :host_real_long, :created_at, :updated_at, :user_id, :review_id
 
   belongs_to :user, serializer: Users::BookingsSerializer
-  has_one :review, serializer: Reviews::Serializer
 
   def host_id
     host = User.where(nickname: object.host_nickname)
@@ -33,6 +32,12 @@ class Bookings::IndexSerializer < ActiveModel::Serializer
     host = User.where(nickname: object.host_nickname)
     unless host.length == 0
       return host[0].profile_avatar.attached? ? (Rails.env.test? ? rails_blob_url(host[0].profile_avatar) : host[0]&.profile_avatar&.service_url(expires_in: 1.hour, disposition: 'inline')) : nil
+    end
+  end
+
+  def review_id
+    unless object.review == nil
+      return object.review.id
     end
   end
 
