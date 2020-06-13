d7e7744fff6913504b0c902e0631bc9be10f406c
raptorf1 <raptor_f1@hotmail.com>
2020-06-13 18:01:13 +0300

    Removes review association from host profiles show serializers. Adjusts relevant request spec.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/serializers/host_profiles/show_serializer.rb
+++ app/serializers/host_profiles/show_serializer.rb
@@ -3,6 +3,5 @@ class HostProfiles::ShowSerializer < ActiveModel::Serializer
   attributes :id, :description, :full_address, :price_per_day_1_cat, :supplement_price_per_cat_per_day, :max_cats_accepted, :availability, :forbidden_dates, :score
 
   belongs_to :user, serializer: Users::Serializer
-  has_many :review, serializer: Reviews::Serializer
 
 end
