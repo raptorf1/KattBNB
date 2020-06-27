0370bae4e9e9a51111433823551606b1077e9e75
raptorf1 <raptor_f1@hotmail.com>
2020-06-27 19:46:24 +0300

    Creates new mailer method to notify host on new review of a booking. Creates relevant mailer view. Implements translations. Adjusts Reviews controller.
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ app/mailers/reviews_mailer.rb
@@ -0,0 +1,17 @@
+class ReviewsMailer < ApplicationMailer
+
+  def notify_host_create_review(host, booking, user, review)
+    @booking = booking
+    @review = review
+    @host = host
+    @user = user
+
+    @start_date = Time.at(booking.dates[0] / 1000)
+    @end_date = Time.at(booking.dates[booking.dates.length - 1] / 1000)
+
+    I18n.with_locale(@host.lang_pref) do
+      mail(to: @host.email, subject: I18n.t('mailers.reviews.notify_host_create_review'))
+    end
+  end
+
+end
