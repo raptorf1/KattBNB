889f8029ff168546d9db4bca51cafbd19a9be36a
raptorf1 <raptor_f1@hotmail.com>
2020-07-02 21:06:47 +0300

    Creates view for the third and final mailer of user pending review notification. Implements translations. Creates spec. 6/6 examples passing!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/mailers/reviews_spec.rb
+++ spec/mailers/reviews_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe ReviewsMailer, type: :mailer do
   let(:new_review_mail) { ReviewsMailer.notify_host_create_review(host, booking, user, review) }
   let(:pending_review_1_day) { ReviewsMailer.notify_user_pending_review_1_day(host, user, booking) }
   let(:pending_review_3_days) { ReviewsMailer.notify_user_pending_review_3_days(host, user, booking) }
+  let(:pending_review_10_days) { ReviewsMailer.notify_user_pending_review_10_days(host, user, booking) }
 
   describe 'notify_host_create_review' do
     it 'renders the subject' do
@@ -93,6 +94,35 @@ RSpec.describe ReviewsMailer, type: :mailer do
 
     it 'performs at least 800K iterations per second' do
       expect { pending_review_3_days }.to perform_at_least(800000).ips
+    end
+  end
+
+  describe 'notify_user_pending_review_10_days' do
+    it 'renders the subject' do
+      expect(pending_review_10_days.subject).to eql("Leave a review for #{host.nickname}")
+    end
+
+    it 'renders the receiver email' do
+      expect(pending_review_10_days.to).to eql([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(pending_review_10_days.from).to eql('KattBNB meow-reply')
+    end
+
+    it 'contains basic information' do
+      expect(pending_review_10_days.body.encoded).to match("Hey, #{user.nickname}!")
+      expect(pending_review_10_days.body.encoded).to match("#{host.nickname}")
+      expect(pending_review_10_days.body.encoded).to match('Positive or negative feedback - we want to hear it!')
+      expect(pending_review_10_days.body.encoded).to match('If you have feedback you are not comfortable sharing on kattbnb.se you are always welcome to reach out to us via')
+    end
+
+    it 'is performed under 600ms' do
+      expect { pending_review_10_days }.to perform_under(600).ms.sample(20).times
+    end
+
+    it 'performs at least 800K iterations per second' do
+      expect { pending_review_10_days }.to perform_at_least(800000).ips
     end
   end
 
