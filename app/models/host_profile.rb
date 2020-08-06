a982df11c692e2f89d4a2698338dec5276f393f3
raptorf1 <raptor_f1@hotmail.com>
2020-08-06 19:33:27 +0300

    Implements before_create callback in HostProfile model to assign value in stripe_state field. Adds test for that. Test passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/models/host_profile.rb
+++ app/models/host_profile.rb
@@ -1,7 +1,16 @@
 class HostProfile < ApplicationRecord
+
+  before_create :assign_stripe_state
+
   belongs_to :user
 
   has_many :review, dependent: :nullify
 
   validates_presence_of :description, :full_address, :price_per_day_1_cat, :supplement_price_per_cat_per_day, :max_cats_accepted
+
+
+  def assign_stripe_state
+    self.stripe_state = SecureRandom.hex + self.user.nickname
+  end
+
 end
