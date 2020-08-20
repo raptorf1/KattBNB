5dd5c611dff43e52eea303ed665f0ac19a398be6
raptorf1 <raptor_f1@hotmail.com>
2020-08-20 20:29:42 +0300

    Sad path when host has no Stripe account
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/controllers/api/v1/stripe_controller.rb
+++ app/controllers/api/v1/stripe_controller.rb
@@ -6,12 +6,16 @@ class Api::V1::StripeController < ApplicationController
     profile = HostProfile.where(id: params[:host_profile_id])
     if current_api_v1_user.id == profile[0].user_id && params[:host_profile_id]
       stripe_account = profile[0].stripe_account_id
-      begin
-        Stripe.api_key = ENV['OFFICIAL'] == 'yes' ? Rails.application.credentials.STRIPE_API_KEY_PROD : Rails.application.credentials.STRIPE_API_KEY_DEV
-        response = Stripe::Account.retrieve(stripe_account)
-        render json: { payouts_enabled: response.payouts_enabled, requirements: response.requirements }, status: 200
-      rescue Stripe::StripeError
-        render json: { error: I18n.t('controllers.reusable.stripe_error') }
+      if stripe_account
+        begin
+          Stripe.api_key = ENV['OFFICIAL'] == 'yes' ? Rails.application.credentials.STRIPE_API_KEY_PROD : Rails.application.credentials.STRIPE_API_KEY_DEV
+          response = Stripe::Account.retrieve(stripe_account)
+          render json: { payouts_enabled: response.payouts_enabled, requirements: response.requirements }, status: 200
+        rescue Stripe::StripeError
+          render json: { error: I18n.t('controllers.reusable.stripe_error') }
+        end
+      else
+        render json: { message: 'No account' }, status: 200
       end
     else
       render json: { error: I18n.t('controllers.reusable.update_error') }, status: 422
