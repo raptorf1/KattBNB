e12e71527b0d24eb6206716add8d47a8ddac6a56
raptorf1 <raptor_f1@hotmail.com>
2020-09-14 19:26:41 +0300

    Adds elsif statement to retrieve login link from Stripe API. Refactors use of Stripe API key. Tests sad path.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/controllers/api/v1/stripe_controller.rb
+++ app/controllers/api/v1/stripe_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::StripeController < ApplicationController
   def index
     profile = HostProfile.where(id: params[:host_profile_id])
     stripe_account = profile[0].stripe_account_id
+    Stripe.api_key = ENV['OFFICIAL'] == 'yes' ? Rails.application.credentials.STRIPE_API_KEY_PROD : Rails.application.credentials.STRIPE_API_KEY_DEV
     if current_api_v1_user.id == profile[0].user_id && params[:occasion] == 'retrieve'
       if stripe_account
         begin
-          Stripe.api_key = ENV['OFFICIAL'] == 'yes' ? Rails.application.credentials.STRIPE_API_KEY_PROD : Rails.application.credentials.STRIPE_API_KEY_DEV
           response = Stripe::Account.retrieve(stripe_account)
           render json: { payouts_enabled: response.payouts_enabled, requirements: response.requirements }, status: 200
         rescue Stripe::StripeError
@@ -18,7 +18,14 @@ class Api::V1::StripeController < ApplicationController
         render json: { message: 'No account' }, status: 200
       end
     elsif current_api_v1_user.id == profile[0].user_id && params[:occasion] == 'login_link'
-      # do some stuff
+      if stripe_account
+        begin
+          response = Stripe::Account.create_login_link(stripe_account)
+          render json: { url: response.url }, status: 200
+        rescue Stripe::StripeError
+          render json: { error: I18n.t('controllers.reusable.stripe_error') }, status: 500
+        end
+      end
     else
       render json: { error: I18n.t('controllers.reusable.update_error') }, status: 422
     end
