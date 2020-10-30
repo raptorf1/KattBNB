363945971f133349604fe378210e49bba53c45a2
raptorf1 <raptor_f1@hotmail.com>
2020-10-30 19:10:54 +0200

    Addresses PR comments from @LauraRe
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- spec/tasks/pay_the_host.spec.rb
+++ spec/tasks/pay_the_host.spec.rb
@@ -7,33 +7,33 @@ describe 'rake bookings:pay_the_host', type: :task do
   let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
   let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
   let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id, stripe_account_id: 'acct_1HSQspCYqtPdaLmE') }
-  let!(:booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456], paid: false, price_total: 1.0) }
-  let!(:booking2) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456, 9563148800000], paid: false) }
-  let!(:booking3) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'pending', paid: false) }
-  let!(:booking4) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'declined', paid: false) }
-  let!(:booking5) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'canceled', paid: false) }
-  let!(:booking6) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456], paid: true) }
+  let!(:unpaid_accepted_booking_past) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456], paid: false, price_total: 1.0) }
+  let!(:unpaid_accepted_booking_future) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456, 9563148800000], paid: false) }
+  let!(:pending_booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'pending', paid: false) }
+  let!(:declined_booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'declined', paid: false) }
+  let!(:canceled_booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'canceled', paid: false) }
+  let!(:paid_accepted_booking) { FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'accepted', dates: [123, 456], paid: true) }
 
   it 'pays the host and updates the correct booking' do
-    expect(booking.paid).to eq false
-    expect(booking2.paid).to eq false
-    expect(booking3.paid).to eq false
-    expect(booking4.paid).to eq false
-    expect(booking5.paid).to eq false
-    expect(booking6.paid).to eq true
+    expect(unpaid_accepted_booking_past.paid).to eq false
+    expect(unpaid_accepted_booking_future.paid).to eq false
+    expect(pending_booking.paid).to eq false
+    expect(declined_booking.paid).to eq false
+    expect(canceled_booking.paid).to eq false
+    expect(paid_accepted_booking.paid).to eq true
     task.execute
-    booking.reload
-    booking2.reload
-    booking3.reload
-    booking4.reload
-    booking5.reload
-    booking6.reload
-    expect(booking.paid).to eq true
-    expect(booking2.paid).to eq false
-    expect(booking3.paid).to eq false
-    expect(booking4.paid).to eq false
-    expect(booking5.paid).to eq false
-    expect(booking6.paid).to eq true
+    unpaid_accepted_booking_past.reload
+    unpaid_accepted_booking_future.reload
+    pending_booking.reload
+    declined_booking.reload
+    canceled_booking.reload
+    paid_accepted_booking.reload
+    expect(unpaid_accepted_booking_past.paid).to eq true
+    expect(unpaid_accepted_booking_future.paid).to eq false
+    expect(pending_booking.paid).to eq false
+    expect(declined_booking.paid).to eq false
+    expect(canceled_booking.paid).to eq false
+    expect(paid_accepted_booking.paid).to eq true
   end
 
 end
