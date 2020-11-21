182c8b63d3fc60c833258abba1d164fc0038c5ec
raptorf1 <raptor_f1@hotmail.com>
2020-11-21 12:51:45 +0200

    Creates new mailer to gather financial stats from paid bookings. View for that mailer. Spec for that mailer. Adds mailer to pay_the_host task.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

---
+++ app/mailers/reports_mailer.rb
@@ -0,0 +1,12 @@
+class ReportsMailer < ApplicationMailer
+
+  def bookings_revenue_and_vat(booking)
+    id = booking.id
+    @host = booking.host_nickname
+    @days = booking.dates.length
+    @host_price = booking.price_total
+
+    mail(to: 'george@kattbnb.se', subject: "New paid booking with id #{id}")
+  end
+
+end
