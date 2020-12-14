ff7f65e29d92386a165e4548ce2b29289edbf364
raptorf1 <raptor_f1@hotmail.com>
2020-12-14 18:06:37 +0200

    Adds another test for deleting Stripe account that does not exist. All 11 examples passing.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- spec/requests/api/v1/stripe/index_spec.rb
+++ spec/requests/api/v1/stripe/index_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::StripeController, type: :request do
   let(:credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
   let(:user2) { FactoryBot.create(:user, email: 'felix@mail.com', nickname: 'MacOS') }
+  let!(:profile_user2) { FactoryBot.create(:host_profile, user_id: user2.id, stripe_account_id: 'acct_wTfAyD65545$mf') }
   let(:credentials2) { user2.create_new_auth_token }
   let(:headers2) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials2) }
   let(:not_headers) { { HTTP_ACCEPT: 'application/json' } }
@@ -67,6 +68,12 @@ RSpec.describe Api::V1::StripeController, type: :request do
         get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: headers
         expect(response.status).to eq 200
         expect(json_response['message']).to eq 'No account'
+      end
+
+      it 'returns custom generic error if user tries to request stripe account deletion of account that does not exist' do
+        get "/api/v1/stripe?host_profile_id=#{profile_user2.id}&occasion=delete_account", headers: headers2
+        expect(response.status).to eq 555
+        expect(json_response['error']).to eq 'There was a problem connecting to our payments infrastructure provider. Please try again later.'
       end
     end
 
