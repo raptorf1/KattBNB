d08303ee3e27591caff63ab0a9b70d883676d2e8
raptorf1 <raptor_f1@hotmail.com>
2021-02-18 19:11:40 +0200

    Creates 1st iteration of index action. New translation for sad path when email is invalid.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- app/controllers/api/v1/contactus_controller.rb
+++ app/controllers/api/v1/contactus_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::ContactusController < ApplicationController
 
   def index
+    if Truemail.valid?(params[:email])
+      render json: { message: 'Success!!!' }, status: 200
+      # new mailer here
+    else
+      render json: { error: [I18n.t('controllers.contactus.create_error')] }, status: 422
+    end
   end
-
 end
