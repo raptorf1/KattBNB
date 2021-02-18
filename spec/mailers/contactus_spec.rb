cb7c95527bfd9aff6f3c516f3462e223c061ad4f
raptorf1 <raptor_f1@hotmail.com>
2021-02-18 22:29:35 +0200

    Creates new view for contactus mailer. Creates spec to test it. All 6 examples passing.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

---
+++ spec/mailers/contactus_spec.rb
@@ -0,0 +1,31 @@
+RSpec::Benchmark.configure { |config| config.run_in_subprocess = true }
+
+RSpec.describe ContactusMailer, type: :mailer do
+  let(:new_visitor_message) { ContactusMailer.send_visitor_message('Richard Dragon', 'rdrag@drop.nl', 'A question for the website owners!') }
+
+    it 'renders the subject' do
+      expect(new_visitor_message.subject).to eql('New visitor message from contact-us form')
+    end
+
+    it 'renders the receiver email' do
+      expect(new_visitor_message.to).to eql(['zane@kattbnb.se'])
+    end
+
+    it 'renders the sender email' do
+      expect(new_visitor_message.from).to eql('KattBNB meow-reply')
+    end
+
+    it 'contains visitor input' do
+      expect(new_visitor_message.body.encoded).to match('Richard Dragon')
+      expect(new_visitor_message.body.encoded).to match('rdrag@drop.nl')
+      expect(new_visitor_message.body.encoded).to match('A question for the website owners!')
+    end
+
+    it 'is performed under 500ms' do
+      expect { new_visitor_message }.to perform_under(500).ms.sample(20).times
+    end
+
+    it 'performs at least 800K iterations per second' do
+      expect { new_visitor_message }.to perform_at_least(800_000).ips
+    end  
+end
