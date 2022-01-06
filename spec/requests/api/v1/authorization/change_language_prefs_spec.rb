a2be868a80031db91cd6dc27ae68ceaa399dc6a3
oliverochman <oliver.ochman@hotmail.com>
2022-01-06 18:32:21 +0100

    refactor update user language preference spec
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- spec/requests/api/v1/authorization/change_language_prefs_spec.rb
+++ spec/requests/api/v1/authorization/change_language_prefs_spec.rb
@@ -2,74 +2,36 @@ RSpec::Benchmark.configure { |config| config.run_in_subprocess = true }
 
 RSpec.describe 'User Changes Language Preference Information and API', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+  let(:user) { FactoryBot.create(:user, lang_pref: 'sv-SE')}
+  let(:auth_token) { user.create_new_auth_token }
 
-  it 'returns a user and a token and then changes the language preference' do
-    post '/api/v1/auth',
-         params: {
-           email: 'felix@craft.se',
-           password: 'password',
-           password_confirmation: 'password',
-           nickname: 'FlavioB',
-           location: 'Tokyo',
-           lang_pref: 'sv-SE',
-           confirm_success_url: 'confirmed'
-         },
-         headers: headers
+  describe 'returns a user and a token and then changes the language preference' do
+    before do
+      @put_request = put '/api/v1/auth',
+      params: {
+        lang_pref: 'en-US',
+        uid: user.uid,
+        'access-token': auth_token['access-token'],
+        client: auth_token['client']
+      },
+      headers: headers
+    end
 
-    User.last.update_attribute(:confirmed_at, '2019-08-10 09:56:34.588757')
+    it 'is expected to return a success message' do      
+      expect(json_response['status']).to eq 'success'
+    end
 
-    post '/api/v1/auth/sign_in', params: { email: 'felix@craft.se', password: 'password' }, headers: headers
+    it "is expected to return a 200 response status" do
+      expect(response.status).to eq 200
+    end
 
-    new_user_uid = response.headers['uid']
-    new_user_token = response.headers['access-token']
-    new_user_client = response.headers['client']
-    new_user_id = json_response['data']['id']
+    it "is expected to return new language preference" do
+      expect(json_response['data']['lang_pref']).to eq 'en-US' 
+    end
 
-    put '/api/v1/auth',
-        params: {
-          lang_pref: 'en-US',
-          uid: new_user_uid,
-          'access-token': new_user_token,
-          client: new_user_client
-        },
-        headers: headers
-    expect(json_response['status']).to eq 'success'
-    expect(response.status).to eq 200
-    expect(json_response['data']['lang_pref']).to eq 'en-US'
-  end
-
-  it 'returns a user and a token and then changes the language preference in under 1 ms and with iteration rate of 2000000 per second' do
-    post '/api/v1/auth',
-         params: {
-           email: 'felix@craft.se',
-           password: 'password',
-           password_confirmation: 'password',
-           nickname: 'FlavioB',
-           location: 'Tokyo',
-           lang_pref: 'sv-SE',
-           confirm_success_url: 'confirmed'
-         },
-         headers: headers
-
-    User.last.update_attribute(:confirmed_at, '2019-08-10 09:56:34.588757')
-
-    post '/api/v1/auth/sign_in', params: { email: 'felix@craft.se', password: 'password' }, headers: headers
-
-    new_user_uid = response.headers['uid']
-    new_user_token = response.headers['access-token']
-    new_user_client = response.headers['client']
-    new_user_id = json_response['data']['id']
-
-    put_request =
-      put '/api/v1/auth',
-          params: {
-            lang_pref: 'en-US',
-            uid: new_user_uid,
-            'access-token': new_user_token,
-            client: new_user_client
-          },
-          headers: headers
-    expect { put_request }.to perform_under(1).ms.sample(20).times
-    expect { put_request }.to perform_at_least(2_000_000).ips
+    it "is expected to change the language preference in under 1 ms and with iteration rate of 2000000 per second" do
+      expect { @put_request }.to perform_under(1).ms.sample(20).times
+      expect { @put_request }.to perform_at_least(2_000_000).ips  
+    end
   end
 end
