ca518e93d27efe5e5085e7bfd72ece01d887c2d4
raptorf1 <raptor_f1@hotmail.com>
2022-01-12 00:42:41 +0200

    refactors delete_unassociated_reviews_spec
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- spec/tasks/delete_unassociated_reviews_spec.rb
+++ spec/tasks/delete_unassociated_reviews_spec.rb
@@ -1,31 +1,33 @@
 describe 'rake reviews:delete_unassociated_reviews', type: :task do
-  let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id) }
-  let!(:booking) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
-  let!(:review) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking.id) }
-  let!(:booking2) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
-  let!(:review2) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking2.id) }
-  let!(:booking3) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
-  let!(:review3) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking3.id) }
-  let!(:booking4) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
-  let!(:review4) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking4.id) }
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
 
-  it 'preloads the Rails environment' do
+  let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id) }
+
+  let(:booking) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
+  let!(:review) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking.id) }
+
+  let(:booking2) { FactoryBot.create(:booking, status: 'accepted', user_id: user.id, host_nickname: host.nickname) }
+  let!(:review2) { FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking2.id) }
+
+  it 'successfully preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
 
-  it 'runs gracefully with no errors' do
-    expect { task.execute }.not_to raise_error
-  end
+  describe 'successfully' do
+    before do
+      review.update_attribute(:user_id, nil)
+      review.update_attribute(:booking_id, nil)
+      review.update_attribute(:host_profile_id, nil)
+      @subject = task.execute
+    end
 
-  it 'logs to stdout' do
-    review.update_attribute(:user_id, nil)
-    review.update_attribute(:booking_id, nil)
-    review.update_attribute(:host_profile_id, nil)
-    review2.update_attribute(:user_id, nil)
-    review2.update_attribute(:booking_id, nil)
-    review2.update_attribute(:host_profile_id, nil)
-    expect { task.execute }.to output("2 unassociated review(s) successfully deleted!\n").to_stdout
+    it 'runs gracefully with no errors' do
+      expect { @subject }.not_to raise_error
+    end
+
+    it 'logs to stdout' do
+      expect(@std_output).to eq("1 unassociated review(s) successfully deleted!\n")
+    end
   end
 end
