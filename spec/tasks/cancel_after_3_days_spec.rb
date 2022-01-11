540e7e267f15aeb1b478802b5159a5ad5466d521
oliverochman <oliver.ochman@hotmail.com>
2022-01-11 19:00:51 +0100

    refactor cancel booking after 3 days task spec
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- spec/tasks/cancel_after_3_days_spec.rb
+++ spec/tasks/cancel_after_3_days_spec.rb
@@ -1,7 +1,8 @@
 describe 'rake bookings:cancel_after_3_days', type: :task do
-  let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:booking) do
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  
+  let!(:cancelled_booking) do
     FactoryBot.create(
       :booking,
       created_at: 'Sat, 09 Nov 2019 09:06:48 UTC +00:00',
@@ -10,28 +11,42 @@ describe 'rake bookings:cancel_after_3_days', type: :task do
       dates: [123, 456]
     )
   end
-  let!(:booking2) do
+
+  let!(:pending_booking) do
     FactoryBot.create(:booking, created_at: Time.current, user_id: user.id, host_nickname: host.nickname)
   end
 
-  it 'emails the user, the host and KattBNB to cancel payment' do
-    task.execute
-    expect(Delayed::Job.all.count).to eq 3
-  end
-
-  it 'preloads the Rails environment' do
+  it 'successfully preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
 
-  it 'runs gracefully with no errors' do
-    expect { task.execute }.not_to raise_error
-  end
+  describe 'successfully' do
+    before do
+      output = StringIO.new
+      $stdout = output
+      @subject = task.execute                   
+    end
 
-  it 'logs to stdout' do
-    expect { task.execute }.to output("1 pending booking(s) succesfully cancelled!\n").to_stdout
-    booking.reload
-    booking2.reload
-    expect(booking.status).to eq 'canceled'
-    expect(booking2.status).to eq 'pending'
+    it 'logs to stdout' do  
+      expect($stdout.string).to eq("1 pending booking(s) succesfully cancelled!\n")
+    end
+
+    it 'changes status of booking to cancelled' do
+      cancelled_booking.reload
+      expect(cancelled_booking.status).to eq 'canceled'
+    end
+
+    it 'does not change status of pending booking' do
+      pending_booking.reload
+      expect(pending_booking.status).to eq 'pending'
+    end
+
+    it 'emails the user, the host and KattBNB to cancel payment' do    
+      expect(Delayed::Job.all.count).to eq 3
+    end
+    
+    it 'runs gracefully with no errors' do
+      expect { @subject }.not_to raise_error
+    end 
   end
 end
