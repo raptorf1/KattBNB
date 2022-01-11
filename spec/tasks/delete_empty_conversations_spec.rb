73cf2b6256f477491c9652222dc8d2e24c162456
oliverochman <oliver.ochman@hotmail.com>
2022-01-11 19:09:38 +0100

    refactor delete empty conversations task spec
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- spec/tasks/delete_empty_conversations_spec.rb
+++ spec/tasks/delete_empty_conversations_spec.rb
@@ -1,29 +1,38 @@
 describe 'rake conversations:delete_empty_conversations', type: :task do
-  let!(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:user3) { FactoryBot.create(:user, email: 'cat@woman.com', nickname: 'Catwoman') }
-  let!(:conversation1) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
-  let!(:conversation2) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user3.id) }
+  let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:user2) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let(:user3) { FactoryBot.create(:user, email: 'cat@woman.com', nickname: 'Catwoman') }
+  let(:conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id) }
+  let!(:empty_conversation) { FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user3.id) }
   let!(:message1) do
-    FactoryBot.create(:message, conversation_id: conversation1.id, user_id: user1.id, body: 'Sweet child of mine!!!!')
+    FactoryBot.create(:message, conversation_id: conversation.id, user_id: user1.id, body: 'Sweet child of mine!!!!')
   end
 
-  it 'preloads the Rails environment' do
+  it 'successfully preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
 
-  it 'runs gracefully with no errors' do
-    expect { task.execute }.not_to raise_error
-  end
+  describe 'successfully' do
+    before do
+      output = StringIO.new
+      $stdout = output
+      @subject = task.execute                   
+    end
 
-  it 'deletes only empty conversations' do
-    expect(Conversation.all.length).to eq 2
-    task.execute
-    expect(Conversation.all.length).to eq 1
-    expect(Conversation.last.id).to eq conversation1.id
-  end
+    it 'runs gracefully with no errors' do
+      expect { @subject }.not_to raise_error
+    end
 
-  it 'logs to stdout' do
-    expect { task.execute }.to output("1 empty conversation(s) succesfully deleted!\n").to_stdout
+    it 'removes the empty conversation' do
+      expect(Conversation.all.length).to eq 1
+    end
+  
+    it 'does not remove active conversation' do
+      expect(Conversation.last.id).to eq conversation.id
+    end
+  
+    it 'logs to stdout' do
+      expect($stdout.string).to eq("1 empty conversation(s) succesfully deleted!\n")
+    end
   end
 end
