f50ea731f97981cf61e954347c81b4af0d26015f
raptorf1 <raptor_f1@hotmail.com>
2022-01-12 18:06:35 +0200

    Refactors task pay_the_host.spec
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- spec/tasks/pay_the_host.spec.rb
+++ spec/tasks/pay_the_host.spec.rb
@@ -1,8 +1,9 @@
-# happy path
 describe 'rake bookings:pay_the_host', type: :task do
-  let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+
   let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id, stripe_account_id: 'acct_1Hywpk2HucmOr4dd') }
+
   let!(:unpaid_accepted_booking_past) do
     FactoryBot.create(
       :booking,
@@ -14,6 +15,7 @@ describe 'rake bookings:pay_the_host', type: :task do
       price_total: 1.0
     )
   end
+
   let!(:unpaid_accepted_booking_future) do
     FactoryBot.create(
       :booking,
@@ -24,15 +26,7 @@ describe 'rake bookings:pay_the_host', type: :task do
       paid: false
     )
   end
-  let!(:pending_booking) do
-    FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'pending', paid: false)
-  end
-  let!(:declined_booking) do
-    FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'declined', paid: false)
-  end
-  let!(:canceled_booking) do
-    FactoryBot.create(:booking, user_id: user.id, host_nickname: host.nickname, status: 'canceled', paid: false)
-  end
+
   let!(:paid_accepted_booking) do
     FactoryBot.create(
       :booking,
@@ -44,63 +38,56 @@ describe 'rake bookings:pay_the_host', type: :task do
     )
   end
 
-  it 'pays the host and updates the correct booking' do
-    expect(unpaid_accepted_booking_past.paid).to eq false
-    expect(unpaid_accepted_booking_future.paid).to eq false
-    expect(pending_booking.paid).to eq false
-    expect(declined_booking.paid).to eq false
-    expect(canceled_booking.paid).to eq false
-    expect(paid_accepted_booking.paid).to eq true
-    task.execute
-    unpaid_accepted_booking_past.reload
-    unpaid_accepted_booking_future.reload
-    pending_booking.reload
-    declined_booking.reload
-    canceled_booking.reload
-    paid_accepted_booking.reload
-    expect(unpaid_accepted_booking_past.paid).to eq true
-    expect(unpaid_accepted_booking_future.paid).to eq false
-    expect(pending_booking.paid).to eq false
-    expect(declined_booking.paid).to eq false
-    expect(canceled_booking.paid).to eq false
-    expect(paid_accepted_booking.paid).to eq true
-  end
-
-  it 'logs to stdout' do
-    expect { task.execute }.to output("A report email was sent!\n").to_stdout
-  end
-end
-
-# sad path
-describe 'rake bookings:pay_the_host', type: :task do
-  let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:profile) { FactoryBot.create(:host_profile, user_id: host.id, stripe_account_id: 'acct_1kdhfsjdhfsfkljsd') }
-  let!(:booking) do
-    FactoryBot.create(
-      :booking,
-      user_id: user.id,
-      host_nickname: host.nickname,
-      status: 'accepted',
-      dates: [123, 456],
-      paid: false,
-      price_total: 1.0
-    )
-  end
-
-  it 'sends email on Stripe error' do
-    task.execute
-    jobs = Delayed::Job.all
-    expect(jobs.count).to eq 1
-    expect(jobs[0].queue).to eq 'stripe_email_notifications'
-    expect(jobs[0].handler).to match("Payment to host for booking id #{booking.id} failed")
-  end
-
   it 'preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
 
-  it 'runs gracefully with no errors' do
-    expect { task.execute }.not_to raise_error
+  describe 'succesfully' do
+    before do
+      @subject = task.execute
+      unpaid_accepted_booking_past.reload
+      unpaid_accepted_booking_future.reload
+      paid_accepted_booking.reload
+    end
+
+    it 'runs gracefully with no errors' do
+      expect { @subject }.not_to raise_error
+    end
+
+    it 'pays the host and updates the correct booking' do
+      expect(unpaid_accepted_booking_past.paid).to eq true
+    end
+
+    it 'does not pay the host of future booking' do
+      expect(unpaid_accepted_booking_future.paid).to eq false
+    end
+
+    it 'does not pay again the host of an already paid booking' do
+      expect(paid_accepted_booking.paid).to eq true
+    end
+
+    it 'logs to stdout' do
+      expect(@std_output).to eq("A report email was sent!\n")
+    end
+  end
+
+  describe 'unsuccessfully' do
+    before do
+      profile.update_attribute(:stripe_account_id, 'acct_1kdhfsjdhfsfkljsd')
+      @subject = task.execute
+      @jobs = Delayed::Job.all
+    end
+
+    it 'sends correct number of emails on Stripe error' do
+      expect(@jobs.count).to eq 1
+    end
+
+    it 'sent email has the correct queue name' do
+      expect(@jobs.first.queue).to eq 'stripe_email_notifications'
+    end
+
+    it 'sent email has the correct booking ID as subject' do
+      expect(@jobs[0].handler).to match("Payment to host for booking id #{unpaid_accepted_booking_past.id} failed")
+    end
   end
 end
