d5c7e82e176fc150b26f012b1e276ddd00ae6e55
raptorf1 <raptor_f1@hotmail.com>
2022-01-12 17:17:49 +0200

    Refactors task notify_pending_review_spec
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- spec/tasks/notify_pending_review_spec.rb
+++ spec/tasks/notify_pending_review_spec.rb
@@ -1,9 +1,11 @@
 describe 'rake reviews:notify_pending_review', type: :task do
   now = DateTime.new(Time.now.year, Time.now.month, Time.now.day, 0, 0, 0, 0)
   now_epoch_javascript = (now.to_f * 1000).to_i
-  let!(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let!(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let!(:booking) do
+
+  let(:user) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+
+  let!(:booking_1_day) do
     FactoryBot.create(
       :booking,
       status: 'accepted',
@@ -12,7 +14,8 @@ describe 'rake reviews:notify_pending_review', type: :task do
       dates: [now_epoch_javascript - 86_400_000]
     )
   end
-  let!(:booking2) do
+
+  let!(:booking_3_days) do
     FactoryBot.create(
       :booking,
       status: 'accepted',
@@ -21,7 +24,8 @@ describe 'rake reviews:notify_pending_review', type: :task do
       dates: [now_epoch_javascript - 86_400_000 * 3]
     )
   end
-  let!(:booking3) do
+
+  let!(:booking_10_days) do
     FactoryBot.create(
       :booking,
       status: 'accepted',
@@ -30,7 +34,8 @@ describe 'rake reviews:notify_pending_review', type: :task do
       dates: [now_epoch_javascript - 86_400_000 * 10]
     )
   end
-  let!(:booking4) do
+
+  let!(:booking_15_days) do
     FactoryBot.create(
       :booking,
       status: 'accepted',
@@ -40,22 +45,29 @@ describe 'rake reviews:notify_pending_review', type: :task do
     )
   end
 
-  it 'emails the user' do
-    task.execute
-    expect(Delayed::Job.all.count).to eq 3
-  end
-
-  it 'preloads the Rails environment' do
+  it 'successfully preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
 
-  it 'runs gracefully with no errors' do
-    expect { task.execute }.not_to raise_error
-  end
+  describe 'successfully' do
+    before { @subject = task.execute }
 
-  it 'logs to stdout' do
-    expect { task.execute }.to output(
-      "1 user(s) notified to leave review after 1 day!\n1 user(s) notified to leave review after 3 days!\n1 user(s) notified to leave review after 10 days!\n"
-    ).to_stdout
+    it 'sends correct number of emails to the user' do
+      expect(Delayed::Job.all.count).to eq 3
+    end
+
+    it 'excludes 15 days old booking' do
+      Delayed::Job.all.each { |email| expect(email.handler[(now_epoch_javascript - 86_400_000 * 15).to_s]).to eq nil }
+    end
+
+    it 'runs gracefully with no errors' do
+      expect { @subject }.not_to raise_error
+    end
+
+    it 'logs to stdout' do
+      expect(@std_output).to eq(
+        "1 user(s) notified to leave review after 1 day!\n1 user(s) notified to leave review after 3 days!\n1 user(s) notified to leave review after 10 days!\n"
+      )
+    end
   end
 end
