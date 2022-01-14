a9f9dac9404f0034f9bf2eb61ecdfd5ce243bcd9
raptorf1 <raptor_f1@hotmail.com>
2022-01-14 18:11:55 +0200

    Refactors stripe/index_spec.rb again
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- spec/requests/api/v1/stripe/index_spec.rb
+++ spec/requests/api/v1/stripe/index_spec.rb
@@ -11,70 +11,66 @@ RSpec.describe 'GET /api/v1/stripe', type: :request do
 
   let(:not_headers) { { HTTP_ACCEPT: 'application/json' } }
 
-  def api_call_with_profile(h_prof_id, occasion, call_headers)
-    get "/api/v1/stripe?host_profile_id=#{h_prof_id}&occasion=#{occasion}", headers: call_headers
-  end
-
   describe 'successfully' do
     it 'with relevant message if no Stripe account is found' do
-      api_call_with_profile(profile_user.id, 'retrieve', headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: headers
       expect(json_response['message']).to eq 'No account'
     end
 
     it 'with 200 status if no Stripe account is found' do
-      api_call_with_profile(profile_user.id, 'retrieve', headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: headers
       expect(response.status).to eq 200
     end
 
     it 'with relevant message if no Stripe account is found when requesting a Stripe account deletion' do
-      api_call_with_profile(profile_user.id, 'delete_account', headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: headers
       expect(json_response['message']).to eq 'No account'
     end
 
     it 'with 200 status if no Stripe account is found when requesting a Stripe account deletion' do
-      api_call_with_profile(profile_user.id, 'delete_account', headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: headers
       expect(response.status).to eq 200
     end
   end
 
   describe 'unsuccesfully' do
     it 'with relevant error if user tries to request stripe profile details of another user' do
-      api_call_with_profile(profile_user.id, 'retrieve', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: headers2
       expect(json_response['error']).to eq 'You cannot perform this action!'
     end
 
     it 'with 422 status if user tries to request stripe profile details of another user' do
-      api_call_with_profile(profile_user.id, 'retrieve', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: headers2
       expect(response.status).to eq 422
     end
 
     it 'with relevant error if user is not authenticated and tries to request own stripe profile details' do
-      api_call_with_profile(profile_user.id, 'retrieve', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: not_headers
       expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
     end
 
     it 'with 401 status if user is not authenticated and tries to request own stripe profile details' do
-      api_call_with_profile(profile_user.id, 'retrieve', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=retrieve", headers: not_headers
       expect(response.status).to eq 401
     end
 
     it 'with relevant error if user tries to request stripe dashboard link of another user' do
-      api_call_with_profile(profile_user.id, 'login_link', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=login_link", headers: headers2
       expect(json_response['error']).to eq 'You cannot perform this action!'
     end
 
     it 'with 422 status if user tries to request stripe dashboard link of another user' do
-      api_call_with_profile(profile_user.id, 'login_link', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=login_link", headers: headers2
       expect(response.status).to eq 422
     end
 
     it 'with relevant error if user is not authenticated and requests stripe dashboard link' do
-      api_call_with_profile(profile_user.id, 'login_link', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=login_link", headers: not_headers
       expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
     end
 
     it 'with 401 status if user is not authenticated and requests stripe dashboard link' do
-      api_call_with_profile(profile_user.id, 'login_link', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=login_link", headers: not_headers
       expect(response.status).to eq 401
     end
 
@@ -99,34 +95,34 @@ RSpec.describe 'GET /api/v1/stripe', type: :request do
     end
 
     it 'with relevant error if user is not authenticated and requests stripe account deletion' do
-      api_call_with_profile(profile_user.id, 'delete_account', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: not_headers
       expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
     end
 
     it 'with 401 status if user is not authenticated and requests stripe account deletion' do
-      api_call_with_profile(profile_user.id, 'delete_account', not_headers)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: not_headers
       expect(response.status).to eq 401
     end
 
     it 'with relevant error if user tries to request stripe account deletion of another user' do
-      api_call_with_profile(profile_user.id, 'delete_account', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: headers2
       expect(json_response['error']).to eq 'You cannot perform this action!'
     end
 
     it 'with 422 status if user tries to request stripe account deletion of another user' do
-      api_call_with_profile(profile_user.id, 'delete_account', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user.id}&occasion=delete_account", headers: headers2
       expect(response.status).to eq 422
     end
 
     it 'with relevant custom error if user tries to request stripe account deletion of account that does not exist' do
-      api_call_with_profile(profile_user2.id, 'delete_account', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user2.id}&occasion=delete_account", headers: headers2
       expect(
         json_response['error']
       ).to eq 'There was a problem connecting to our payments infrastructure provider. Please try again later.'
     end
 
     it 'with 555 custom status if user tries to request stripe account deletion of account that does not exist' do
-      api_call_with_profile(profile_user2.id, 'delete_account', headers2)
+      get "/api/v1/stripe?host_profile_id=#{profile_user2.id}&occasion=delete_account", headers: headers2
       expect(response.status).to eq 555
     end
   end
