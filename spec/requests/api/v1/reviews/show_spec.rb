c876b64751ad2e04d1c518824f58d96493ec4bc6
oliverochman <oliver.ochman@hotmail.com>
2022-01-18 20:10:44 +0100

    refactor show review req spec
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- spec/requests/api/v1/reviews/show_spec.rb
+++ spec/requests/api/v1/reviews/show_spec.rb
@@ -1,46 +1,68 @@
 RSpec.describe 'GET /api/v1/reviews', type: :request do
-  let(:user1) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
-  let(:credentials1) { user1.create_new_auth_token }
-  let(:headers1) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials1) }
-  let(:booking1) { FactoryBot.create(:booking, user_id: user1.id) }
+  let(:reviewer) { FactoryBot.create(:user, email: 'chaos@thestreets.com', nickname: 'Joker') }
+  let(:r_credentials) { reviewer.create_new_auth_token }
+  let(:reviewer_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(r_credentials) }
 
-  let(:user2) { FactoryBot.create(:user, email: 'morechaos@thestreets.com', nickname: 'Harley Quinn') }
-  let(:credentials2) { user2.create_new_auth_token }
-  let(:headers2) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials2) }
-  let(:profile2) { FactoryBot.create(:host_profile, user_id: user2.id) }
-  let(:booking2) { FactoryBot.create(:booking, user_id: user2.id) }
+  let(:host) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
+  let(:host_profile) { FactoryBot.create(:host_profile, user_id: host.id) }
+  let(:h_credentials) { host.create_new_auth_token }
+  let(:host_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(h_credentials) }
 
-  let(:user3) { FactoryBot.create(:user, email: 'order@thestreets.com', nickname: 'Batman') }
-  let(:profile3) { FactoryBot.create(:host_profile, user_id: user3.id) }
+  let(:booking) { FactoryBot.create(:booking, user_id: reviewer.id) }
+  let(:review) do
+    FactoryBot.create(:review, host_profile_id: host_profile.id, user_id: reviewer.id, booking_id: booking.id)
+  end
 
-  let(:review1) { FactoryBot.create(:review, user_id: user1.id, host_profile_id: profile2.id, booking_id: booking1.id) }
-  let(:review2) { FactoryBot.create(:review, user_id: user2.id, host_profile_id: profile3.id, booking_id: booking2.id) }
+  let(:other_credentials) { FactoryBot.create(:user).create_new_auth_token }
+  let(:other_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(other_credentials) }
 
-  let(:not_headers) { { HTTP_ACCEPT: 'application/json' } }
+  let(:unauthenticated_headers) { { HTTP_ACCEPT: 'application/json' } }
 
   describe 'successfully' do
-    before { get "/api/v1/reviews/#{review1.id}", headers: headers1 }
+    describe 'for reviewer' do
+      before { get "/api/v1/reviews/#{review.id}", headers: reviewer_headers }
 
-    it 'with 200 status' do
-      expect(response.status).to eq 200
+      it 'with 200 status' do
+        expect(response.status).to eq 200
+      end
+
+      it 'with correct review id in the response' do
+        expect(json_response['id']).to eq review.id
+      end
+
+      it 'with correct number of keys in the response' do
+        expect(json_response.count).to eq 9
+      end
+
+      it 'with correct keys in the response' do
+        expect(json_response).to include('id', 'score', 'body', 'host_reply', 'host_nickname', 'host_avatar', 'user')
+      end
     end
 
-    it 'with correct review id in the response' do
-      expect(json_response['id']).to eq review1.id
-    end
+    describe 'for host' do
+      before { get "/api/v1/reviews/#{review.id}", headers: host_headers }
 
-    it 'with correct number of keys in the response' do
-      expect(json_response.count).to eq 9
-    end
+      it 'with 200 status' do
+        expect(response.status).to eq 200
+      end
 
-    it 'with correct keys in the response' do
-      expect(json_response).to include('id', 'score', 'body', 'host_reply', 'host_nickname', 'host_avatar', 'user')
+      it 'with correct review id in the response' do
+        expect(json_response['id']).to eq review.id
+      end
+
+      it 'with correct number of keys in the response' do
+        expect(json_response.count).to eq 9
+      end
+
+      it 'with correct keys in the response' do
+        expect(json_response).to include('id', 'score', 'body', 'host_reply', 'host_nickname', 'host_avatar', 'user')
+      end
     end
   end
 
   describe 'unsuccessfully' do
     describe 'if not logged in' do
-      before { get "/api/v1/reviews/#{review1.id}", headers: not_headers }
+      before { get "/api/v1/reviews/#{review.id}", headers: unauthenticated_headers }
 
       it 'with relevant error' do
         expect(json_response['errors']).to eq ['You need to sign in or sign up before continuing.']
@@ -52,7 +74,7 @@ RSpec.describe 'GET /api/v1/reviews', type: :request do
     end
 
     describe 'if not part of the review' do
-      before { get "/api/v1/reviews/#{review1.id}", headers: headers2 }
+      before { get "/api/v1/reviews/#{review.id}", headers: other_headers }
 
       it 'with relevant error' do
         expect(json_response['error']).to eq ['You cannot perform this action!']
