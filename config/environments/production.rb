bf398c9db9d65a5df930a19b4dfdf8240c840b68
raptorf1 <raptor_f1@hotmail.com>
2022-01-22 16:33:31 +0200

    Yet another attempt!
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,20 +57,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.office365.com',
     port: '587',
-    domain: 'gmail.com',
-    authentication: :plain,
+    domain: 'office365.com',
+    authentication: :login,
     enable_starttls_auto: true,
-    user_name: ENV['OFFICIAL'] == 'yes' ? 'meow-reply@kattbnb.se' : 'kattbnb@gmail.com',
-    password:
-      if ENV['OFFICIAL'] == 'yes'
-        Rails.application.credentials.GMAIL_PASSWORD_PROD
-      else
-        Rails.application.credentials.GMAIL_PASSWORD_DEV
-      end
+    user_name: 'meow-reply@kattbnb.se',
+    password: Rails.application.credentials.MS365_PASSWORD_PROD
   }
-  config.action_mailer.default_options = { from: 'KattBNB meow-reply' }
+  config.action_mailer.default_options = { from: 'meow-reply@kattbnb.se' }
   config.action_mailer.default_url_options = { host: ENV['API_ENDPOINT'] }
 
   config.action_mailer.perform_caching = false
