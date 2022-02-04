e0c0e7148e480837dc34daf0fd771dde05cbbdc5
raptorf1 <raptor_f1@hotmail.com>
2022-02-04 12:18:38 +0200

    Edits bookings controller update action to test happy path. Refactors relevant request spec.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- spec/requests/api/v1/bookings/patch_spec.rb
+++ spec/requests/api/v1/bookings/patch_spec.rb
@@ -1,51 +1,43 @@
 RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
-  let(:host1) { FactoryBot.create(:user, email: 'george@mail.com', nickname: 'Alonso') }
-  let(:credentials_host1) { host1.create_new_auth_token }
-  let(:headers_host1) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_host1) }
-  let!(:profile1) do
+  let(:host) { FactoryBot.create(:user) }
+  let(:credentials_host) { host.create_new_auth_token }
+  let(:headers_host) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_host) }
+  let!(:host_profile) do
     FactoryBot.create(
       :host_profile,
-      user_id: host1.id,
-      availability: [1_562_803_200_000, 1_563_062_400_000, 1_563_148_800_000],
-      full_address: 'Arlanda Airport',
-      description: 'It is a really fucking nice airport',
-      latitude: 52.365598,
-      longitude: 3.321478221
+      user_id: host.id,
+      availability: [1_562_803_200_000, 1_563_062_400_000, 1_563_148_800_000]
     )
   end
 
-  let(:host2) { FactoryBot.create(:user, email: 'noel@craft.com', nickname: 'MacOS') }
-  let(:credentials_host2) { host2.create_new_auth_token }
-  let(:headers_host2) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_host2) }
-
-  let(:user1) { FactoryBot.create(:user, email: 'faraz@craft.com', nickname: 'EarlyInTheMorning') }
-  let(:credentials_user1) { user1.create_new_auth_token }
-  let(:headers_user1) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_user1) }
+  let(:cat_owner) { FactoryBot.create(:user) }
+  let(:credentials_cat_owner) { cat_owner.create_new_auth_token }
+  let(:headers_cat_owner) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_cat_owner) }
 
   let(:booking) do
     FactoryBot.create(
       :booking,
-      host_nickname: host1.nickname,
-      user_id: user1.id,
-      dates: [1_562_889_600_000, 1_562_976_000_000]
+      host_nickname: host.nickname,
+      user_id: cat_owner.id,
+      dates: [1_562_803_200_000, 1_563_062_400_000]
     )
   end
 
-  let!(:booking2) do
+  let!(:accepted_booking) do
     FactoryBot.create(
       :booking,
-      host_nickname: host1.nickname,
-      user_id: user1.id,
+      host_nickname: host.nickname,
+      user_id: cat_owner.id,
       status: 'accepted',
       dates: [1_662_976_000_000, 2_662_976_000_000]
     )
   end
 
-  let(:booking3) do
-    FactoryBot.create(:booking, host_nickname: host1.nickname, user_id: user1.id, dates: [1_662_976_000_000])
+  let(:cancelled_booking) do
+    FactoryBot.create(:booking, host_nickname: host.nickname, user_id: cat_owner.id, dates: [1_662_976_000_000])
   end
 
-  let(:headers_no_auth) { { HTTP_ACCEPT: 'application/json' } }
+  let(:unauthenticated_headers) { { HTTP_ACCEPT: 'application/json' } }
 
   def patch_request(book_id, book_status, book_message, book_headers)
     patch "/api/v1/bookings/#{book_id}",
@@ -56,36 +48,100 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
           headers: book_headers
   end
 
-  describe 'unsuccessfully' do
-    describe 'cause of Stripe error when capturing the money' do
+  describe 'succesfully' do
+    describe 'when host accepts' do
       before do
-        patch_request(booking.id, 'accepted', 'accepted by host', headers_host1)
+        patch_request(booking.id, 'accepted', 'yeah baby!!!! cat wars!', headers_host)
         booking.reload
+        host_profile.reload
       end
 
-      it 'with 555 status' do
-        expect(response.status).to eq 555
+      it 'with 200 status' do
+        expect(response.status).to eq 200
       end
 
-      it 'with relevant error' do
-        expect(
-          json_response['error']
-        ).to eq 'There was a problem connecting to our payments infrastructure provider. Please try again later.'
+      it 'with relevant message' do
+        expect(json_response['message']).to eq 'You have successfully updated this booking!'
       end
 
       it 'with correct booking status' do
-        expect(booking.status).to eq 'pending'
+        expect(booking.status).to eq 'accepted'
       end
 
       it 'with correct booking host_message' do
-        expect(booking.host_message).to eq nil
+        expect(booking.host_message).to eq 'yeah baby!!!! cat wars!'
+      end
+
+      it 'with correct booking host_description' do
+        expect(booking.host_description).to eq host_profile.description
+      end
+
+      it 'with correct booking host_full_address' do
+        expect(booking.host_full_address).to eq host_profile.full_address
+      end
+
+      it 'with correct booking host_real_lat' do
+        expect(booking.host_real_lat).to eq host_profile.latitude
+      end
+
+      it 'with correct booking host_real_long' do
+        expect(booking.host_real_long).to eq host_profile.longitude
+      end
+
+      it 'with updated host profile availability' do
+        expect(host_profile.availability).to eq [1_563_148_800_000]
+      end
+
+      it 'with correct number of emails sent' do
+        expect(Delayed::Job.all.count).to eq 1
+      end
+
+      it 'with email to user about accepted booking' do
+        expect(Delayed::Job.first.handler.include?('notify_user_accepted_booking')).to eq true
       end
     end
 
+    describe 'when host declines' do
+      before do
+        patch_request(booking.id, 'declined', 'sorry', headers_host)
+        booking.reload
+      end
+
+      it 'with 200 status' do
+        expect(response.status).to eq 200
+      end
+
+      it 'with relevant message' do
+        expect(json_response['message']).to eq 'You have successfully updated this booking!'
+      end
+
+      it 'with correct booking status' do
+        expect(booking.status).to eq 'declined'
+      end
+
+      it 'with correct booking host_message' do
+        expect(booking.host_message).to eq 'sorry'
+      end
+
+      it 'with correct number of emails sent' do
+        expect(Delayed::Job.all.count).to eq 2
+      end
+
+      it 'with email to user about declined booking' do
+        expect(Delayed::Job.first.handler.include?('notify_user_declined_booking')).to eq true
+      end
+
+      it 'with email to KattBNB about Stripe payment intent' do
+        expect(Delayed::Job.last.handler.include?('notify_orphan_payment_intent_to_cancel')).to eq true
+      end
+    end
+  end
+
+  describe 'unsuccessfully' do
     describe 'if another booking has already been accepted on the same date range' do
       before do
-        patch_request(booking3.id, 'accepted', 'glad to be of assistance!', headers_host1)
-        booking3.reload
+        patch_request(cancelled_booking.id, 'accepted', 'glad to be of assistance!', headers_host)
+        cancelled_booking.reload
       end
 
       it 'with 427 status' do
@@ -111,12 +167,12 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
       end
 
       it 'with correct status on booking' do
-        expect(booking3.status).to eq 'canceled'
+        expect(cancelled_booking.status).to eq 'canceled'
       end
 
       it 'with correct message on booking' do
         expect(
-          booking3.host_message
+          cancelled_booking.host_message
         ).to eq 'This booking got canceled by KattBNB. The host has accepted another booking in that date range.'
       end
     end
@@ -127,7 +183,7 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
           booking.id,
           'accepted',
           'accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host accepted by host',
-          headers_host1
+          headers_host
         )
         booking.reload
       end
@@ -149,20 +205,8 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
       end
     end
 
-    describe 'if action comes from an unassociated host' do
-      before { patch_request(booking.id, 'declined', 'declined by host', headers_host2) }
-
-      it 'with 422 status' do
-        expect(response.status).to eq 422
-      end
-
-      it 'with relevant error' do
-        expect(json_response['error']).to eq 'You cannot perform this action!'
-      end
-    end
-
-    describe 'even if action comes from the user that primarily requested the booking' do
-      before { patch_request(booking.id, 'accepted', 'accepted by host', headers_user1) }
+    describe 'if action comes from the user that primarily requested the booking / an unassociated host' do
+      before { patch_request(booking.id, 'accepted', 'accepted by host', headers_cat_owner) }
 
       it 'with 422 status' do
         expect(response.status).to eq 422
@@ -174,7 +218,7 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
     end
 
     describe 'if user is not authenticated' do
-      before { patch_request(booking.id, '', '', headers_no_auth) }
+      before { patch_request(booking.id, '', '', unauthenticated_headers) }
 
       it 'with 401 status' do
         expect(response.status).to eq 401
@@ -186,7 +230,7 @@ RSpec.describe 'PATCH /api/v1/bookings/id', type: :request do
     end
 
     describe 'if booking does not exist' do
-      before { patch '/api/v1/bookings/2000000000', headers: headers_user1 }
+      before { patch '/api/v1/bookings/2000000000', headers: headers_cat_owner }
 
       it 'with custom 404 status' do
         expect(response.status).to eq 404
