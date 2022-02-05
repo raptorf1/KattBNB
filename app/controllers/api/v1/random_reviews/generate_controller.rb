10b9cb1eeead088d23ece4b35598b3a034055b23
raptorf1 <raptor_f1@hotmail.com>
2022-02-05 11:55:41 +0200

    Completes code of controller and relevant request spec
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- app/controllers/api/v1/random_reviews/generate_controller.rb
+++ app/controllers/api/v1/random_reviews/generate_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::RandomReviews::GenerateController < ApplicationController
-
   def index
-    render json: { message: 'yay!!!' }, status: 200
-    #params[:host_profile_id] ? reviews = Review.where(host_profile_id: params[:host_profile_id]) : reviews = []
-    #render json: reviews, each_serializer: Reviews::Serializer
+    high_score_reviews = Review.where(score: 5)
+    if high_score_reviews.length > 2
+      render json: high_score_reviews.shuffle.first(3), each_serializer: Reviews::Serializer, status: 200
+    else
+      render json: { error: 'Not enough 5 paw reviews!' }, status: 404
+    end
   end
 end
--- spec/requests/api/v1/random_reviews/generate/index_spec.rb
+++ spec/requests/api/v1/random_reviews/generate/index_spec.rb
@@ -1,17 +1,35 @@
 RSpec.describe 'GET /api/v1/random_reviews/generate', type: :request do
-  let(:host_profile) { FactoryBot.create(:host_profile) }
-  let!(:reviews) { 10.times { FactoryBot.create(:review, host_profile_id: host_profile.id) } }
+  let!(:high_score_reviews) { 5.times { FactoryBot.create(:review, score: 5) } }
+  let!(:low_score_review) { FactoryBot.create(:review) }
 
-  describe 'successfully' do
+  describe 'successfully: when more than 2 reviews exist' do
     before { get '/api/v1/random_reviews/generate' }
 
-      it 'with 200 status' do
-        binding.pry
-        expect(response.status).to eq 200
-      end
-    
-    
+    it 'with 200 status' do
+      expect(response.status).to eq 200
+    end
+
+    it 'with correct number of reviews' do
+      expect(json_response.length).to eq 3
+    end
+
+    it 'with only 5 paw reviews' do
+      json_response.each { |review| expect(review['id']).not_to eq low_score_review.id }
+    end
   end
 
-  
+  describe 'unsuccessfully: when less than 3 reviews exist' do
+    before do
+      Review.all.destroy_all
+      get '/api/v1/random_reviews/generate'
+    end
+
+    it 'with 404 status' do
+      expect(response.status).to eq 404
+    end
+
+    it 'with relevant error' do
+      expect(json_response['error']).to eq 'Not enough 5 paw reviews!'
+    end
+  end
 end
