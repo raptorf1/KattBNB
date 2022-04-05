a2a1ea8b5a7035929279e6d22a2081424da8509d
raptorf1 <raptor_f1@hotmail.com>
2022-04-05 13:06:45 +0300

    Refactors model conversation spec
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Conversation, type: :model do
   it 'should have a valid Factory' do
+    User.destroy_all
     expect(create(:conversation)).to be_valid
   end
 
@@ -26,24 +27,10 @@ RSpec.describe Conversation, type: :model do
   end
 
   describe 'Delete dependent setting' do
-    it 'conversation is nullified when associated user is deleted from the database' do
-      user1 = FactoryBot.create(:user, email: 'george@cyprus.com', nickname: 'george')
-      user2 = FactoryBot.create(:user, email: 'zane@sweden.com', nickname: 'zane')
-      user3 = FactoryBot.create(:user, email: 'boa@norway.com', nickname: 'boa')
-      conversation1 = FactoryBot.create(:conversation, user1_id: user1.id, user2_id: user2.id)
-      conversation2 = FactoryBot.create(:conversation, user1_id: user3.id, user2_id: user1.id)
-      conversation3 = FactoryBot.create(:conversation, user1_id: user2.id, user2_id: user3.id)
-      expect(Conversation.all.length).to eq 3
-      expect(User.all.length).to eq 3
-      user1.destroy
-      conversation1.reload
-      conversation2.reload
-      expect(Conversation.all.length).to eq 3
-      expect(User.all.length).to eq 2
-      expect(conversation1.user1_id).to eq nil
-      expect(conversation1.user2_id).to eq user2.id
-      expect(conversation2.user1_id).to eq user3.id
-      expect(conversation2.user2_id).to eq nil
+    it 'message is deleted when associated conversation is deleted from the database' do
+      FactoryBot.create(:message)
+      Message.last.conversation.destroy
+      expect(Message.all.length).to eq 0
     end
   end
 end
