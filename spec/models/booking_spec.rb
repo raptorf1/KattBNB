928641bd3191fbf7234e0d7e9dbe911c7ee1cd9f
raptorf1 <raptor_f1@hotmail.com>
2022-04-05 13:20:02 +0300

    Refactors model booking spec
    Co-authored-by: zane <zane.neikena@gmail.com>

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Booking, type: :model do
   it 'should have valid Factory' do
+    User.destroy_all
     expect(create(:booking)).to be_valid
   end
 
@@ -7,6 +8,7 @@ RSpec.describe Booking, type: :model do
     it { is_expected.to have_db_column :number_of_cats }
     it { is_expected.to have_db_column :message }
     it { is_expected.to have_db_column :host_message }
+    it { is_expected.to have_db_column :user_id }
     it { is_expected.to have_db_column :status }
     it { is_expected.to have_db_column :host_nickname }
     it { is_expected.to have_db_column :dates }
@@ -17,6 +19,9 @@ RSpec.describe Booking, type: :model do
     it { is_expected.to have_db_column :host_real_lat }
     it { is_expected.to have_db_column :host_real_long }
     it { is_expected.to have_db_column :payment_intent_id }
+    it { is_expected.to have_db_column :host_profile_id }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
     it { is_expected.to have_db_column :paid }
   end
 
@@ -29,8 +34,8 @@ RSpec.describe Booking, type: :model do
     it { is_expected.to validate_presence_of :price_per_day }
     it { is_expected.to validate_presence_of :price_total }
     it { is_expected.to validate_presence_of :user_id }
-    it { is_expected.to validate_length_of :message }
-    it { is_expected.to validate_length_of :host_message }
+    it { is_expected.to validate_length_of(:message).is_at_most(400) }
+    it { is_expected.to validate_length_of(:host_message).is_at_most(200) }
   end
 
   describe 'Associations' do
@@ -39,6 +44,10 @@ RSpec.describe Booking, type: :model do
 
   describe 'Relations' do
     it { is_expected.to have_one(:review) }
+  end
+
+  describe 'enum definitions' do
+    it { should define_enum_for :status }
   end
 
   describe 'Default values' do
@@ -54,55 +63,10 @@ RSpec.describe Booking, type: :model do
   end
 
   describe 'Delete dependent setting' do
-    it 'canceled booking is deleted when associated user is deleted from the database' do
-      FactoryBot.create(:booking, status: 'canceled')
-      expect(Booking.all.length).to eq 1
-      expect(User.all.length).to eq 1
-      User.last.destroy
-      expect(Booking.all.length).to eq 0
-      expect(User.all.length).to eq 0
-    end
-
-    it 'user is not deleted when associated declined booking is deleted from the database' do
-      FactoryBot.create(:booking, status: 'declined')
-      expect(Booking.all.length).to eq 1
-      expect(User.all.length).to eq 1
-      Booking.last.destroy
-      expect(Booking.all.length).to eq 0
-      expect(User.all.length).to eq 1
-    end
-
-    it 'accepted past booking is deleted when associated user is deleted from the database' do
-      user1 = FactoryBot.create(:user, email: 'george@mail.com', nickname: 'Alonso')
-      user2 = FactoryBot.create(:user, email: 'zane@mail.com', nickname: 'Kitten')
-      booking =
-        FactoryBot.create(
-          :booking,
-          host_nickname: user2.nickname,
-          user_id: user1.id,
-          status: 'accepted',
-          dates: [1_462_889_600_000, 1_462_976_000_000]
-        )
-      user1.destroy
-      expect(Booking.all.length).to eq 0
-    end
-
     it 'review is nullified when associated booking is deleted' do
-      user = FactoryBot.create(:user, email: 'george@mail.com', nickname: 'Alonso')
-      host = FactoryBot.create(:user, email: 'zane@mail.com', nickname: 'Kitten')
-      profile = FactoryBot.create(:host_profile, user_id: host.id)
-      booking =
-        FactoryBot.create(
-          :booking,
-          host_nickname: host.nickname,
-          user_id: user.id,
-          status: 'accepted',
-          dates: [1_462_889_600_000, 1_462_976_000_000]
-        )
-      review = FactoryBot.create(:review, user_id: user.id, host_profile_id: profile.id, booking_id: booking.id)
-      booking.destroy
-      review.reload
-      expect(review.booking_id).to eq nil
+      FactoryBot.create(:review)
+      Review.last.booking.destroy
+      expect(Review.last.booking_id).to eq nil
     end
   end
 end
