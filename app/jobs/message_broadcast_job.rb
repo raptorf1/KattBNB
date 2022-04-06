a000707ce4e8e1c9e8a4e08d036c9e7e5a4ff31f
raptorf1 <raptor_f1@hotmail.com>
2022-04-06 16:05:19 +0300

    Refactors jobs
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,12 +2,10 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :messages
 
   def perform(message_id)
-    message = Message.find_by(id: message_id)
-    if message
-      serialized_data = ActiveModelSerializers::Adapter::Json.new(Messages::Serializer.new(message)).serializable_hash
-      ActionCable.server.broadcast("conversations_#{message.conversation.id}_channel", serialized_data)
-    else
-      return I18n.t('jobs.message_broadcast.error', message: message_id)
-    end
+    message = Message.find(message_id)
+    serialized_data = ActiveModelSerializers::Adapter::Json.new(Messages::Serializer.new(message)).serializable_hash
+    ActionCable.server.broadcast("conversations_#{message.conversation.id}_channel", serialized_data)
+  rescue ActiveRecord::RecordNotFound
+    return I18n.t('jobs.message_broadcast.error', message: message_id)
   end
 end
