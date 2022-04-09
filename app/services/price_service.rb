3bf72dbf0900bc8ac0852eed8c63d9f6740f9b0c
raptorf1 <raptor_f1@hotmail.com>
2022-04-09 17:41:04 +0300

    Code formatting
    Co-authored-by: zane <zane.neikena@gmail.com>

--- app/services/price_service.rb
+++ app/services/price_service.rb
@@ -1,11 +1,7 @@
 module PriceService
   def self.two_decimals_converter(number)
     string_with_2_decimals = sprintf('%.2f', number.to_s)
-    if string_with_2_decimals.last(2) == '00'
-      string_with_2_decimals.to_i
-    else
-      sprintf('%.2f', string_with_2_decimals)
-    end
+    string_with_2_decimals.last(2) == '00' ? string_with_2_decimals.to_i : sprintf('%.2f', string_with_2_decimals)
   end
 
   def self.calculate_kattbnb_charge(number)
