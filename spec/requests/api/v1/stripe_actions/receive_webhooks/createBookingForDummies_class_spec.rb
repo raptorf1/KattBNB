f488452ad972f2bc2445f789be5a494a23e2a393
raptorf1 <raptor_f1@hotmail.com>
2022-11-07 16:56:23 +0200

    Adds another sad path scenario for CreateBookingForDummies class

--- spec/requests/api/v1/stripe_actions/receive_webhooks/createBookingForDummies_class_spec.rb
+++ spec/requests/api/v1/stripe_actions/receive_webhooks/createBookingForDummies_class_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "POST /api/v1/stripe_actions/receive_webhooks", type: :request do
     )
   end
 
-  describe "for CreateBookingForDummies class" do
+  describe "testing CreateBookingForDummies class" do
     before { Delayed::Worker.delay_jobs = false }
 
     describe "when booking already exists" do
@@ -28,6 +28,22 @@ RSpec.describe "POST /api/v1/stripe_actions/receive_webhooks", type: :request do
 
       it "with correct log message" do
         expect(@std_output).to eq("Booking already exists! Show me the moneyyyyy!")
+      end
+    end
+
+    describe "when booking does not exist" do
+      before { Booking.destroy_all }
+
+      describe "and during creation is not persisted" do
+        before do
+          post "/api/v1/stripe_actions/receive_webhooks",
+               params: File.open("spec/fixtures/stripe_webhook_events/charge_succeeded_0_dates.json"),
+               headers: headers
+        end
+
+        it "with correct log message" do
+          expect(Booking.all.length).to eq 0
+        end
       end
     end
   end
