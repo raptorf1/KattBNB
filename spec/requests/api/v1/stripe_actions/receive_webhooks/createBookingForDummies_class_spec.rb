aa7519f6a9b8c9abbcf9aee4f1a1e196c7ce200e
raptorf1 <raptor_f1@hotmail.com>
2022-11-08 14:39:41 +0200

    Completes sad path tests for CreateBookingForDummies class. Tests passing.

--- spec/requests/api/v1/stripe_actions/receive_webhooks/createBookingForDummies_class_spec.rb
+++ spec/requests/api/v1/stripe_actions/receive_webhooks/createBookingForDummies_class_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe "POST /api/v1/stripe_actions/receive_webhooks", type: :request do
   let(:headers) { { HTTP_ACCEPT: "application/json" } }
 
-  let(:host) { FactoryBot.create(:user) }
-  let(:host_profile) { FactoryBot.create(:host_profile, user_id: host.id) }
+  let!(:host) { FactoryBot.create(:user) }
+  let!(:host_profile) { FactoryBot.create(:host_profile, user_id: host.id) }
 
-  let(:cat_owner) { FactoryBot.create(:user) }
+  let!(:cat_owner) { FactoryBot.create(:user) }
 
   let!(:booking) do
     FactoryBot.create(
       :booking,
       host_nickname: host.nickname,
       user_id: cat_owner.id,
-      dates: [1_562_803_200_000, 1_563_062_400_000],
-      payment_intent_id: "pi_000000000000000000000000"
+      dates: [1_562_803_200_000, 1_563_062_400_000, 1_666_915_200_000],
+      payment_intent_id: "pi_000000000000000000000000",
+      status: "accepted"
     )
   end
 
@@ -59,12 +60,34 @@ RSpec.describe "POST /api/v1/stripe_actions/receive_webhooks", type: :request do
         end
       end
 
-      describe "and is created but host does not exist in the database" do
+      describe "and is created but host does not exist in the database so it gets deleted" do
         before do
           file =
             FileService.generate_charge_succeeded_stripe_event(
               "1666915200000,1667433600000",
               "NotExistingHost",
+              cat_owner.id,
+              "pi_000000000000000000000001"
+            )
+          post "/api/v1/stripe_actions/receive_webhooks", params: file, headers: headers
+        end
+
+        it "with correct number of bookings in the database" do
+          expect(Booking.all.length).to eq 1
+        end
+
+        it "with correct booking ID in the database" do
+          expect(Booking.all.first.id).to eq booking.id
+        end
+      end
+
+      describe "and is created but host already has an accepted booking in those dates in the database so it gets deleted" do
+        before do
+          booking.update(dates: booking.dates.push(4_133_973_599_999))
+          file =
+            FileService.generate_charge_succeeded_stripe_event(
+              "1666915200000,1667433600000",
+              host.nickname,
               cat_owner.id,
               "pi_000000000000000000000001"
             )
