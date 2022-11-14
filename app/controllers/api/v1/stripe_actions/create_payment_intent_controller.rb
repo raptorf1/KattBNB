e53887e2d6ad5c74328681ba0138a69ef532ca25
raptorf1 <raptor_f1@hotmail.com>
2022-11-14 12:28:09 +0200

    Refactors create_payment_intent controller. Adjusts relevant tests. All passing.

--- app/controllers/api/v1/stripe_actions/create_payment_intent_controller.rb
+++ app/controllers/api/v1/stripe_actions/create_payment_intent_controller.rb
@@ -3,8 +3,10 @@ class Api::V1::StripeActions::CreatePaymentIntentController < ApplicationControl
 
   def index
     Stripe.api_key = StripeService.get_api_key
+
     api_amount = calculate_price(params[:inDate], params[:outDate], params[:cats], params[:host]).to_f
     client_amount = ("%.2f" % params[:amount]).to_f
+
     if api_amount - client_amount <= 1 && api_amount - client_amount >= -1
       stripe_amount = calculate_stripe_amount(params[:amount])
       begin
@@ -12,17 +14,20 @@ class Api::V1::StripeActions::CreatePaymentIntentController < ApplicationControl
           Stripe::PaymentIntent.create(
             {
               amount: stripe_amount,
-              currency: params[:currency],
+              currency: params[:currency].nil? ? "sek" : params[:currency],
               receipt_email: current_api_v1_user.email,
               capture_method: "manual"
             }
           )
         render json: { intent_id: intent.client_secret }, status: 200
-      rescue Stripe::StripeError
-        render json: { error: I18n.t("controllers.reusable.stripe_error") }, status: 555
+      rescue Stripe::StripeError => error
+        render json: { errors: [error.error.message] }, status: 400
       end
     else
-      render json: { error: I18n.t("controllers.reusable.stripe_error") }, status: 555
+      render json: {
+               errors: [I18n.t("controllers.stripe_actions.create_payment_intent_calculate_amount_error")]
+             },
+             status: 400
     end
   end
 
@@ -30,9 +35,9 @@ class Api::V1::StripeActions::CreatePaymentIntentController < ApplicationControl
 
   def calculate_price(in_date, out_date, cats, host)
     user = User.find_by(nickname: host)
-    if user != nil
+    if !user.nil?
       host_profile = HostProfile.find_by(user_id: user.id)
-      if host_profile != nil
+      if !host_profile.nil?
         price = host_profile.price_per_day_1_cat + ((cats.to_i - 1) * host_profile.supplement_price_per_cat_per_day)
         total = price * (((out_date.to_i - in_date.to_i) / 86_400_000) + 1)
         final_charge = PriceService.calculate_kattbnb_charge(total)
