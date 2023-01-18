d9359b4af8ad7dd59dd13118aa219a4a89113a7c
raptorf1 <raptor_f1@hotmail.com>
2023-01-18 21:18:20 +0100

    Refactors create action. Adjusts relevant tests. Passing.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -7,7 +7,6 @@ class Api::V1::ReviewsController < ApplicationController
 
   def show
     review = Review.find(params[:id])
-
     if current_api_v1_user.id != review.user_id && current_api_v1_user.host_profile&.id != review.host_profile_id
       render json: { errors: [I18n.t("controllers.reusable.update_error")] }, status: 400 and return
     end
@@ -16,29 +15,19 @@ class Api::V1::ReviewsController < ApplicationController
   end
 
   def create
-    now = DateTime.new(Time.now.year, Time.now.month, Time.now.day, 0, 0, 0, 0)
-    now_epoch_javascript = (now.to_f * 1000).to_i
-    review = Review.create(review_params)
-    if review.persisted?
-      booking = Booking.find(params[:booking_id])
-      if current_api_v1_user.id == booking.user_id && booking.status == "accepted" &&
-           booking.dates.last < now_epoch_javascript
-        host = User.where(nickname: booking.host_nickname)
-        user = User.where(id: booking.user_id)
-        render json: { message: I18n.t("controllers.reusable.create_success") }, status: 200
-        ReviewsMailer.delay(queue: "reviews_email_notifications").notify_host_create_review(
-          host[0],
-          booking,
-          user[0],
-          review
-        )
-      else
-        review.destroy
-        render json: { error: [I18n.t("controllers.reusable.update_error")] }, status: 422
-      end
-    else
-      render json: { error: review.errors.full_messages }, status: 422
+    booking = Booking.find(params[:booking_id])
+    if current_api_v1_user.id != booking.user_id || booking.dates.last > DateService.get_js_epoch
+      render json: { errors: [I18n.t("controllers.reusable.update_error")] }, status: 400 and return
     end
+
+    review = Review.create(review_params)
+    render json: { errors: review.errors.full_messages }, status: 400 and return if !review.persisted?
+
+    host = User.find_by(nickname: booking.host_nickname)
+    user = User.find(booking.user_id)
+
+    render json: { message: I18n.t("controllers.reusable.create_success") }, status: 200
+    ReviewsMailer.delay(queue: "reviews_email_notifications").notify_host_create_review(host, booking, user, review)
   end
 
   def update
