8786dd674f26ab7d0b03d39656aad5ca8146c443
raptorf1 <raptor_f1@hotmail.com>
2023-01-20 13:22:23 +0100

    Adds equality in date check

--- app/services/booking_service.rb
+++ app/services/booking_service.rb
@@ -6,7 +6,7 @@ module BookingService
 
     host_booked_dates = []
     host_bookings.each do |host_booking|
-      host_booking.dates.last > DateService.get_js_epoch && host_booked_dates.push(host_booking.dates)
+      host_booking.dates.last >= DateService.get_js_epoch && host_booked_dates.push(host_booking.dates)
     end
     booking_dates - host_booked_dates.flatten.sort == booking_dates
   end
