81154906690f37dd333f2f25bf5b7185f9f298d2
raptorf1 <raptor_f1@hotmail.com>
2023-02-03 16:44:14 +0200

    Returns no content when host profile does not exist in incoming_upcoming_controller. Adjusts specs. Passing.

--- app/controllers/api/v1/fetch_booking_actions/incoming_upcoming_controller.rb
+++ app/controllers/api/v1/fetch_booking_actions/incoming_upcoming_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::FetchBookingActions::IncomingUpcomingController < ApplicationCont
   before_action :authenticate_api_v1_user!, only: %i[index]
 
   def index
-    render json: [], status: 200 and return if current_api_v1_user.host_profile.nil?
+    head :no_content, status: 204 and return if current_api_v1_user.host_profile.nil?
 
     accepted_bookings = Booking.where(status: "accepted", host_nickname: current_api_v1_user.nickname)
     upcoming_bookings = accepted_bookings.select { |booking| booking.dates.last >= DateService.get_js_epoch }
--- spec/requests/api/v1/fetch_booking_actions/incoming_upcoming/index_spec.rb
+++ spec/requests/api/v1/fetch_booking_actions/incoming_upcoming/index_spec.rb
@@ -81,12 +81,12 @@ RSpec.describe "GET /api/v1/fetch_booking_actions/incoming_upcoming", type: :req
     describe "when host profile does not exist" do
       before { get "/api/v1/fetch_booking_actions/incoming_upcoming", headers: random_user_headers }
 
-      it "with 200 status" do
-        expect(response.status).to eq 200
+      it "with 204 status" do
+        expect(response.status).to eq 204
       end
 
-      it "with empty collection as response" do
-        expect(json_response.size).to eq 0
+      it "with empty response body" do
+        expect(response.body).to eq ""
       end
     end
   end
